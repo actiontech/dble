e63525a862ae47b1537154d9b5b9643041e0a91e
yanhuqing666 <yanhuqing666@gmail.com>
2017-01-18 14:28:50 +0800

    fix 25

--- src/main/java/io/mycat/route/parser/druid/impl/DruidInsertParser.java
+++ src/main/java/io/mycat/route/parser/druid/impl/DruidInsertParser.java
@@ -426,18 +426,39 @@ public class DruidInsertParser extends DefaultDruidParser {
 
 		List<SQLExpr> dku = insert.getDuplicateKeyUpdate();
 		if (dku != null && dku.size() > 0) {
+			boolean flag = false;
 			sb.append(" on duplicate key update ");
 			for (int i = 0; i < dku.size(); i++) {
 				SQLExpr exp = dku.get(i);
-				if (exp != null) {
-					if (i < dku.size() - 1)
-						sb.append(exp.toString()).append(",");
-					else
-						sb.append(exp.toString());
+				if(!(exp instanceof SQLBinaryOpExpr)){
+					String msg = "not supported! on duplicate key update exp is "+exp.getClass();
+					LOGGER.warn(msg);
+					throw new SQLNonTransientException(msg);
 				}
+				SQLBinaryOpExpr binaryOpExpr = (SQLBinaryOpExpr)exp;
+				if(!flag && GlobalTableUtil.GLOBAL_TABLE_MYCAT_COLUMN.equals(binaryOpExpr.getLeft().toString())) {
+					flag = true;
+					onDuplicateGlobalColumn(sb);
+				} else {
+					sb.append(binaryOpExpr.toString());
+				}
+				if (i < dku.size() - 1) {
+					sb.append(",");
+				}
+			}
+			if (!flag) {
+				sb.append(",");
+				onDuplicateGlobalColumn(sb);
 			}
 		}
 		return sb.toString();
+	}
+
+	private static void onDuplicateGlobalColumn(StringBuilder sb){
+		sb.append(GlobalTableUtil.GLOBAL_TABLE_MYCAT_COLUMN);
+		sb.append("=values(");
+		sb.append(GlobalTableUtil.GLOBAL_TABLE_MYCAT_COLUMN);
+		sb.append(")");
 	}
 
 	private static StringBuilder appendValues(List<SQLExpr> valuse, StringBuilder sb, int idx, int colSize) {

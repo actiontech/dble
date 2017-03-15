ce8b747e05b1d477784452f8c4758903d11186f6
ditdb <guoji.ma@gmail.com>
2017-03-15 15:50:03 +0800

    #issue-33

---
+++ src/main/java/io/mycat/net/handler/ManagerCommandHandler.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2013, OpenCloudDB/MyCAT and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software;Designed and Developed mainly by many Chinese 
+ * opensource volunteers. you can redistribute it and/or modify it under the 
+ * terms of the GNU General Public License version 2 only, as published by the
+ * Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 
+ * Any questions about this component can be directed to it's project Web address 
+ * https://code.google.com/p/opencloudb/.
+ *
+ */
+package io.mycat.net.handler;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import io.mycat.backend.mysql.MySQLMessage;
+import io.mycat.config.ErrorCode;
+import io.mycat.net.FrontendConnection;
+import io.mycat.net.NIOHandler;
+import io.mycat.net.mysql.MySQLPacket;
+import io.mycat.statistic.CommandCount;
+
+/**
+ * manager命令处理器
+ *
+ * @author mycat
+ */
+public class ManagerCommandHandler implements NIOHandler
+{
+	protected final ConcurrentLinkedQueue<byte[]> dataQueue = new ConcurrentLinkedQueue<byte[]>();
+	protected final AtomicBoolean handleStatus;
+	protected final FrontendConnection source;
+	protected final CommandCount commands;
+
+    public ManagerCommandHandler(FrontendConnection source)
+    {
+        this.source = source;
+        this.commands = source.getProcessor().getCommands();
+        this.handleStatus = new AtomicBoolean(false);
+    }
+
+    @Override
+    public void handle(byte[] data)
+    {
+        if (dataQueue.offer(data)) {
+			handleQueue();
+		} else {
+			throw new RuntimeException("add data to queue error.");
+		}
+    }
+    private void handleData(byte[] data) {
+		switch (data[4]) {
+		case MySQLPacket.COM_QUERY:
+			commands.doQuery();
+			source.query(data);
+			break;
+		case MySQLPacket.COM_PING:
+			commands.doPing();
+			source.ping();
+			break;
+		case MySQLPacket.COM_QUIT:
+			commands.doQuit();
+			source.close("quit cmd");
+			break;
+		default:
+			commands.doOther();
+			source.writeErrMessage(ErrorCode.ER_UNKNOWN_COM_ERROR, "Unknown command");
+		}
+    }
+
+	private void handleQueue() {
+		if (this.handleStatus.compareAndSet(false, true)) {
+			this.source.getProcessor().getExecutor().execute(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						byte[] data = null;
+						while ((data = dataQueue.poll()) != null) {
+							handleData(data);
+						}
+					} catch (Exception e) {
+						source.writeErrMessage(ErrorCode.ER_UNKNOWN_ERROR, e.toString());
+						// QUESTION_TO 阻止当前线程继续工作
+						dataQueue.clear();
+					} finally {
+						handleStatus.set(false);
+						if (dataQueue.size() > 0) {
+							handleQueue();
+						}
+					}
+				}
+			});
+		}
+	}
+}

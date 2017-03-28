3d571e18807f5498067ee11e114318c2fb248bdb
sunsun314 <sunsun3141@gmail.com>
2017-03-28 14:30:06 +0800

    #69 create Forbidden partition & Engine & DATA DIRECTORY & AUTO_INCREMENT

--- src/main/java/io/mycat/route/parser/druid/impl/ddl/DruidCreateTableParser.java
+++ src/main/java/io/mycat/route/parser/druid/impl/ddl/DruidCreateTableParser.java
@@ -2,6 +2,7 @@ package io.mycat.route.parser.druid.impl.ddl;
 
 import com.alibaba.druid.sql.ast.SQLName;
 import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.ast.expr.SQLIdentifierExpr;
 import com.alibaba.druid.sql.ast.statement.SQLColumnDefinition;
 import com.alibaba.druid.sql.ast.statement.SQLCreateTableStatement;
 import com.alibaba.druid.sql.ast.statement.SQLTableElement;
@@ -24,18 +25,9 @@ public class DruidCreateTableParser extends DefaultDruidParser {
 	public SchemaConfig visitorParse(SchemaConfig schema, RouteResultset rrs, SQLStatement stmt, MycatSchemaStatVisitor visitor)
 			throws SQLNonTransientException {
 		MySqlCreateTableStatement createStmt = (MySqlCreateTableStatement)stmt;
+		//创建新表select from禁止
 		if(createStmt.getSelect() != null) {
 			String msg = "create table from other table not supported :" + stmt;
-			LOGGER.warn(msg);
-			throw new SQLNonTransientException(msg);
-		}
-		if(createStmt.getPartitioning() != null){
-			String msg = "create table with Partition not supported:" + stmt;
-			LOGGER.warn(msg);
-			throw new SQLNonTransientException(msg);
-		}
-		if(createStmt.getTableOptions().get("ENGINE")!=null){
-			String msg = "create table with ENGINE  not supported:" + stmt;
 			LOGGER.warn(msg);
 			throw new SQLNonTransientException(msg);
 		}
@@ -45,6 +37,13 @@ public class DruidCreateTableParser extends DefaultDruidParser {
 			String msg = "No MyCAT Database is selected Or defined, sql:" + stmt;
 			throw new SQLNonTransientException(msg);
 		}
+
+		//如果这个不是no_sharing表格那么就需要这么进行检查
+		if(schema.getTables().get(createStmt.getTableSource().toString()).getRule() != null){
+			sharingTableCheck(createStmt);
+		}
+
+
 		if (GlobalTableUtil.useGlobleTableCheck()
 				&& GlobalTableUtil.isGlobalTable(schemaInfo.schemaConfig, schemaInfo.table)) {
 			String sql= addColumnIfCreate(rrs.getStatement(), createStmt);
@@ -75,6 +74,40 @@ public class DruidCreateTableParser extends DefaultDruidParser {
 					break;
 				}
 			}
+		}
+	}
+
+	/**
+	 * 检查创建的表格里面是不是有我们不支持的参数
+	 * @throws SQLNonTransientException
+	 */
+	private void sharingTableCheck(MySqlCreateTableStatement createStmt) throws SQLNonTransientException {
+		//创建新表分片属性禁止
+		if(createStmt.getPartitioning() != null){
+			String msg = "create table with Partition not supported:" + createStmt;
+			LOGGER.warn(msg);
+			throw new SQLNonTransientException(msg);
+		}
+
+		//创建的新表只允许出现InnoDB引擎
+		SQLIdentifierExpr engineConf = (SQLIdentifierExpr)createStmt.getTableOptions().get("ENGINE");
+		if(engineConf != null && !"InnoDB".equalsIgnoreCase(engineConf.getName())){
+			String msg = "create table only can use ENGINE InnoDB,others not supported:" + createStmt;
+			LOGGER.warn(msg);
+			throw new SQLNonTransientException(msg);
+		}
+		//创建新表的时候数据目录指定禁止
+		if (createStmt.getTableOptions().get("DATA DIRECTORY") != null){
+			String msg = "create table with DATA DIRECTORY  not supported:" + createStmt;
+			LOGGER.warn(msg);
+			throw new SQLNonTransientException(msg);
+		}
+
+		// 创建新表的时候自增属性禁止
+		if(createStmt.getTableOptions().get("AUTO_INCREMENT") != null){
+			String msg = "create table with AUTO_INCREMENT not supported:" + createStmt;
+			LOGGER.warn(msg);
+			throw new SQLNonTransientException(msg);
 		}
 	}
 }

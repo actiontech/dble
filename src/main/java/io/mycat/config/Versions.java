834b4863e529ac757066337eefe2dbe9a8b5038a
yanhuqing666 <yanhuqing666@gmail.com>
2017-05-03 16:06:30 +0800

    #117 1.client login msg

--- src/main/java/io/mycat/config/Versions.java
+++ src/main/java/io/mycat/config/Versions.java
@@ -33,7 +33,7 @@ public abstract class Versions {
 
     /**服务器版本**/
     public static byte[] SERVER_VERSION = "5.6.29-mycat-2.17.04.0-20170420011014".getBytes();
-
+    public static byte[] VERSION_COMMENT = "MyCat Server (OpenCloundDB)".getBytes();
     public static void setServerVersion(String version) {
         byte[] mysqlVersionPart = version.getBytes();
         int startIndex;

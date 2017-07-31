1b2c8ba7d42dd16a2baf1ed7a3697dd3efd1e3b1
ditdb <guoji.ma@gmail.com>
2017-07-31 13:30:12 +0800

    this test must setup mycat server, because it need to get metadata of table

--- src/test/java/io/mycat/plan/visitor/TestMySQLPlanNodeVisitor.java
+++ src/test/java/io/mycat/plan/visitor/TestMySQLPlanNodeVisitor.java
@@ -9,17 +9,21 @@ import com.alibaba.druid.sql.parser.SQLStatementParser;
 
 import io.mycat.plan.PlanNode;
 
+/**
+ * this test must setup mycat server, because TableNode need the metadata of tables.
+ * Curent, we skip it.
+ */
 public class TestMySQLPlanNodeVisitor {
 	@Test
 	public void testNoraml() {
-		 PlanNode tableNode = getPlanNode("select * from table1");
+		 PlanNode tableNode = getPlanNode("select * from tvistor");
 		 System.out.println(tableNode);
 		 Assert.assertEquals(true, tableNode !=null  );
 	}
 	private PlanNode getPlanNode(String sql){
 		SQLStatementParser parser = new MySqlStatementParser(sql);
 		SQLSelectStatement ast = (SQLSelectStatement)parser.parseStatement();
-		MySQLPlanNodeVisitor visitor = new MySQLPlanNodeVisitor("test",33);
+		MySQLPlanNodeVisitor visitor = new MySQLPlanNodeVisitor("TESTDB", 33);
 		visitor.visit(ast);
 		return visitor.getTableNode();
 	}

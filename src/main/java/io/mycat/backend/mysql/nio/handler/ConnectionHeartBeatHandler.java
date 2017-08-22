f8929614e72f3a104a857780f6fde6d4a69c347e
yanhuqing666 <yanhuqing666@gmail.com>
2017-08-22 10:16:35 +0800

    heartbeat :remove finished flag

--- src/main/java/io/mycat/backend/mysql/nio/handler/ConnectionHeartBeatHandler.java
+++ src/main/java/io/mycat/backend/mysql/nio/handler/ConnectionHeartBeatHandler.java
@@ -24,19 +24,16 @@
 package io.mycat.backend.mysql.nio.handler;
 
 import io.mycat.backend.BackendConnection;
-import io.mycat.net.mysql.ErrorPacket;
+import io.mycat.backend.mysql.nio.MySQLConnection;
 import io.mycat.net.mysql.FieldPacket;
+import io.mycat.net.mysql.PingPacket;
 import io.mycat.net.mysql.RowDataPacket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map.Entry;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -47,58 +44,141 @@ import java.util.concurrent.locks.ReentrantLock;
 public class ConnectionHeartBeatHandler implements ResponseHandler {
     private static final Logger LOGGER = LoggerFactory
             .getLogger(ConnectionHeartBeatHandler.class);
-    protected final ReentrantLock lock = new ReentrantLock();
-    private final ConcurrentMap<Long, HeartBeatCon> allCons = new ConcurrentHashMap<Long, HeartBeatCon>();
 
-    public void doHeartBeat(BackendConnection conn, String sql) {
+    protected final ReentrantLock lock = new ReentrantLock();
+    final Condition condition = lock.newCondition();
+
+    private final long validateTime = 2;
+
+
+    public void doHeartBeat(BackendConnection conn) {
         if (LOGGER.isDebugEnabled()) {
             LOGGER.debug("do heartbeat for con " + conn);
         }
-
+        lock.lock();
         try {
-
-            HeartBeatCon hbCon = new HeartBeatCon(conn);
-            boolean notExist = (allCons.putIfAbsent(hbCon.conn.getId(), hbCon) == null);
-            if (notExist) {
-                conn.setResponseHandler(this);
-                conn.query(sql);
-
+            conn.setResponseHandler(this);
+            MySQLConnection mcon = (MySQLConnection) conn;
+            mcon.write(mcon.writeToBuffer(PingPacket.PING, mcon.allocate()));
+            if (!condition.await(validateTime, TimeUnit.SECONDS)) {
+                //if the thread be waked up by timer than close the connection
+                conn.close("heartbeat timeout ");
             }
         } catch (Exception e) {
             executeException(conn, e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+
+    /**
+     * if the query returns ok than just release the connection
+     * and go on check the next one
+     *
+     * @param ok
+     * @param conn
+     */
+    @Override
+    public void okResponse(byte[] ok, BackendConnection conn) {
+        lock.lock();
+        try {
+            condition.signal();
+            conn.release();
+        } finally {
+            lock.unlock();
         }
     }
 
     /**
-     * remove timeout connections
+     * if heart beat returns error than clase the connection and
+     * start the next one
+     *
+     * @param data
+     * @param conn
      */
-    public void abandTimeOuttedConns() {
-        if (allCons.isEmpty()) {
-            return;
+    @Override
+    public void errorResponse(byte[] data, BackendConnection conn) {
+        lock.lock();
+        try {
+            condition.signal();
+            conn.close("heatbeat return error");
+        } finally {
+            lock.unlock();
         }
-        Collection<BackendConnection> abandCons = new LinkedList<BackendConnection>();
-        long curTime = System.currentTimeMillis();
-        Iterator<Entry<Long, HeartBeatCon>> itors = allCons.entrySet()
-                .iterator();
-        while (itors.hasNext()) {
-            HeartBeatCon hbCon = itors.next().getValue();
-            if (hbCon.timeOutTimestamp < curTime) {
-                abandCons.add(hbCon.conn);
-                itors.remove();
-            }
-        }
+    }
 
-        if (!abandCons.isEmpty()) {
-            for (BackendConnection con : abandCons) {
-                try {
-                    // if(con.isBorrowed())
-                    con.close("heartbeat timeout ");
-                } catch (Exception e) {
-                    LOGGER.warn("close err:" + e);
-                }
-            }
+    /**
+     * if the heartbeat throws the Exception than close the connection
+     *
+     * @param c
+     * @param e
+     */
+    private void executeException(BackendConnection c, Throwable e) {
+        lock.lock();
+        try {
+            condition.signal();
+            c.close("heatbeat exception:" + e);
+            LOGGER.warn("executeException   ", e);
+        } finally {
+            lock.unlock();
         }
+    }
 
+    /**
+     * if when the query going on the conneciton be closed
+     * than just do nothing and go on for next one
+     *
+     * @param conn
+     * @param reason
+     */
+    @Override
+    public void connectionClose(BackendConnection conn, String reason) {
+        lock.lock();
+        try {
+            condition.signal();
+            LOGGER.warn("connection closed " + conn + " reason:" + reason);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * @param eof
+     * @param isLeft
+     * @param conn
+     */
+    @Override
+    public void rowEofResponse(byte[] eof, boolean isLeft, BackendConnection conn) {
+        // not called
+    }
+
+
+    @Override
+    public void fieldEofResponse(byte[] header, List<byte[]> fields, List<FieldPacket> fieldPackets, byte[] eof,
+                                 boolean isLeft, BackendConnection conn) {
+        // not called
+    }
+
+    @Override
+    public boolean rowResponse(byte[] rownull, RowDataPacket rowPacket, boolean isLeft, BackendConnection conn) {
+        // not called
+        return false;
+    }
+
+    @Override
+    public void writeQueueAvailable() {
+        // not called
+    }
+
+    @Override
+    public void relayPacketResponse(byte[] relayPacket, BackendConnection conn) {
+        // not called
+    }
+
+    @Override
+    public void endPacketResponse(byte[] endPacket, BackendConnection conn) {
+        // not called
     }
 
     @Override
@@ -109,94 +189,5 @@ public class ConnectionHeartBeatHandler implements ResponseHandler {
     @Override
     public void connectionError(Throwable e, BackendConnection conn) {
         // not called
-
     }
-
-    @Override
-    public void errorResponse(byte[] data, BackendConnection conn) {
-        removeFinished(conn);
-        ErrorPacket err = new ErrorPacket();
-        err.read(data);
-        LOGGER.warn("errorResponse " + err.errno + " "
-                + new String(err.message));
-        conn.release();
-
-    }
-
-    @Override
-    public void okResponse(byte[] ok, BackendConnection conn) {
-        boolean executeResponse = conn.syncAndExcute();
-        if (executeResponse) {
-            removeFinished(conn);
-            conn.release();
-        }
-
-    }
-
-    @Override
-    public boolean rowResponse(byte[] rownull, RowDataPacket rowPacket, boolean isLeft, BackendConnection conn) {
-        return false;
-    }
-
-    @Override
-    public void rowEofResponse(byte[] eof, boolean isLeft, BackendConnection conn) {
-        removeFinished(conn);
-        conn.release();
-    }
-
-    private void executeException(BackendConnection c, Throwable e) {
-        removeFinished(c);
-        LOGGER.warn("executeException   ", e);
-        c.close("heatbeat exception:" + e);
-
-    }
-
-    private void removeFinished(BackendConnection con) {
-        Long id = ((BackendConnection) con).getId();
-        this.allCons.remove(id);
-    }
-
-    @Override
-    public void writeQueueAvailable() {
-
-    }
-
-    @Override
-    public void connectionClose(BackendConnection conn, String reason) {
-        removeFinished(conn);
-        LOGGER.warn("connection closed " + conn + " reason:" + reason);
-    }
-
-
-    @Override
-    public void fieldEofResponse(byte[] header, List<byte[]> fields, List<FieldPacket> fieldPackets, byte[] eof,
-                                 boolean isLeft, BackendConnection conn) {
-        if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("received field eof  from " + conn);
-        }
-    }
-
-
-    @Override
-    public void relayPacketResponse(byte[] relayPacket, BackendConnection conn) {
-
-    }
-
-    @Override
-    public void endPacketResponse(byte[] endPacket, BackendConnection conn) {
-
-    }
-
-}
-
-class HeartBeatCon {
-    public final long timeOutTimestamp;
-    public final BackendConnection conn;
-
-    public HeartBeatCon(BackendConnection conn) {
-        super();
-        this.timeOutTimestamp = System.currentTimeMillis() + 20 * 1000L;
-        this.conn = conn;
-    }
-
 }

5d1fc596b72e0029f3858dbabb74671da298487b
sunzhengfang <sunzhengfang@actionsky.com>
2017-11-30 17:29:17 +0800

    #353 Error when excute `drop view if exists` In the case of no view

--- src/main/java/com/actiontech/dble/server/handler/DropViewHandler.java
+++ src/main/java/com/actiontech/dble/server/handler/DropViewHandler.java
@@ -18,12 +18,15 @@ public final class DropViewHandler {
 
     public static void handle(String stmt, ServerConnection c) {
         try {
+            boolean flag = parseViewExists(stmt);
             String[] viewName = parseViewName(stmt);
             //check if all the view is exists
-            for (String singleName : viewName) {
-                if (!(DbleServer.getInstance().getTmManager().getCatalogs().get(c.getSchema()).getViewMetas().containsKey(singleName))) {
-                    c.writeErrMessage(ER_BAD_TABLE_ERROR, " Unknown table '" + singleName + "'");
-                    return;
+            if (!flag) {
+                for (String singleName : viewName) {
+                    if (!(DbleServer.getInstance().getTmManager().getCatalogs().get(c.getSchema()).getViewMetas().containsKey(singleName))) {
+                        c.writeErrMessage(ER_BAD_TABLE_ERROR, " Unknown table '" + singleName + "'");
+                        return;
+                    }
                 }
             }
             for (String singleName : viewName) {
@@ -80,6 +83,52 @@ public final class DropViewHandler {
                             }
                         } else {
                             return stmt.substring(offset - 1, stmt.length()).trim().split(",");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    public static boolean parseViewExists(String sql) throws Exception {
+        //skip the first word of sql 'drop '
+        int offset = 4;
+        String stmt = sql.trim();
+        while (true) {
+            if (!(stmt.charAt(++offset) == ' ' ||
+                    stmt.charAt(offset) == '\t' ||
+                    stmt.charAt(offset) == '\r' ||
+                    stmt.charAt(offset) == '\n')) {
+                //skip  the second words view
+                offset += 4;
+                while (true) {
+                    char t = stmt.charAt(++offset);
+                    if (!(t == ' ' || t == '\t' || t == '\r' || t == '\n')) {
+                        if ((t == 'i' || t == 'I') &&
+                                (stmt.charAt(++offset) == 'f' || stmt.charAt(offset) == 'F')) {
+                            while (true) {
+                                char t1 = stmt.charAt(++offset);
+                                if (!(t1 == ' ' || t1 == '\t' || t1 == '\r' || t1 == '\n')) {
+                                    char c1 = stmt.charAt(offset);
+                                    char c2 = stmt.charAt(++offset);
+                                    char c3 = stmt.charAt(++offset);
+                                    char c4 = stmt.charAt(++offset);
+                                    char c5 = stmt.charAt(++offset);
+                                    char c6 = stmt.charAt(++offset);
+                                    if ((c1 == 'e' || c1 == 'E') &&
+                                            (c2 == 'x' || c2 == 'X') &&
+                                            (c3 == 'i' || c3 == 'I') &&
+                                            (c4 == 's' || c4 == 'S') &&
+                                            (c5 == 't' || c5 == 'T') &&
+                                            (c6 == 's' || c6 == 'S')) {
+                                        return true;
+                                    }
+                                    return false;
+                                }
+                            }
+                        } else {
+                            return false;
                         }
                     }
                 }

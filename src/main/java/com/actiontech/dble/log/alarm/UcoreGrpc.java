370adf7ea3de3600c305f0158890c573d41a5d2f
yanhuqing <yanhuqing666@gmail.com>
2018-01-02 10:00:29 +0800

    Copyright

--- src/main/java/com/actiontech/dble/log/alarm/UcoreGrpc.java
+++ src/main/java/com/actiontech/dble/log/alarm/UcoreGrpc.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2016-2018 ActionTech.
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher.
+ */
+
 package com.actiontech.dble.log.alarm;
 
 import static io.grpc.stub.ClientCalls.asyncUnaryCall;

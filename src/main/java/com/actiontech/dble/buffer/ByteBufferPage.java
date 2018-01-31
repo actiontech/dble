66b644de5639910022df2a8cef33eced0438d86d
sunzhengfang <sunzhengfang@actionsky.com>
2018-01-31 14:51:20 +0800

    #445 Manager command ‘show @@processor’ has the wrong result

--- src/main/java/com/actiontech/dble/buffer/ByteBufferPage.java
+++ src/main/java/com/actiontech/dble/buffer/ByteBufferPage.java
@@ -104,6 +104,6 @@ public class ByteBufferPage {
     }
 
     public long getUsage() {
-        return chunkAllocateTrack.size();
+        return chunkAllocateTrack.cardinality() * chunkSize;
     }
 }

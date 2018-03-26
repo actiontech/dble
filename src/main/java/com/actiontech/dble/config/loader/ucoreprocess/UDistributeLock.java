084d9891af60cfb73d0492187c3f2691866327e7
zhengfang.sun <sunsun3141@gmail.com>
2018-03-26 18:24:03 +0800

    online lock recreate (#486)

--- src/main/java/com/actiontech/dble/config/loader/ucoreprocess/UDistributeLock.java
+++ src/main/java/com/actiontech/dble/config/loader/ucoreprocess/UDistributeLock.java
@@ -1,5 +1,6 @@
 package com.actiontech.dble.config.loader.ucoreprocess;
 
+import com.actiontech.dble.cluster.ClusterParamCfg;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,13 +32,6 @@ public class UDistributeLock {
         ClusterUcoreSender.unlockKey(path, session);
     }
 
-    public void deleteTree() {
-        if (renewThread != null) {
-            renewThread.interrupt();
-        }
-        ClusterUcoreSender.deleteKVTree(this.path + "/");
-    }
-
     public boolean acquire() {
         try {
 
@@ -49,10 +43,15 @@ public class UDistributeLock {
             renewThread = new Thread(new Runnable() {
                 @Override
                 public void run() {
+                    String sessionId = session;
                     while (!Thread.currentThread().isInterrupted()) {
                         try {
                             LOGGER.info("renew lock of session  start:" + sessionId + " " + path);
                             ClusterUcoreSender.renewLock(sessionId);
+                            if (path.equals(UcorePathUtil.getOnlinePath(UcoreConfig.getInstance().getValue(ClusterParamCfg.CLUSTER_CFG_MYID))) &&
+                                    "".equals(ClusterUcoreSender.getKey(path).getValue())) {
+                                sessionId = ClusterUcoreSender.lockKey(path, value);
+                            }
                             LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(10000));
                             LOGGER.info("renew lock of session  success:" + sessionId + " " + path);
                         } catch (Exception e) {

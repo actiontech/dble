09abd92a4031aa3bf4f6fb33fe6f6c407bdcbe4d
sunzhengfang <sunzhengfang@actionsky.com>
2018-04-02 14:00:18 +0800

    add retry in alarm appender

--- src/main/java/com/actiontech/dble/btrace/script/BtraceClusterDelay.java
+++ src/main/java/com/actiontech/dble/btrace/script/BtraceClusterDelay.java
@@ -19,156 +19,142 @@ public final class BtraceClusterDelay {
             method = "delayAfterGetLock"
     )
     public static void delayAfterGetLock(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
-        BTraceUtils.println("get into delayAfterGetLock ");
-        Thread.sleep(10000L);
-    }
+        BTraceUtils.print("get into delayAfterGetLock ");
+        BTraceUtils.print(" for order __________________________ ");
+        Thread.sleep( 10000L);
 
+    }
+/*
     @OnMethod(
             clazz = "com.actiontech.dble.btrace.provider.ClusterDelayProvider",
             method = "delayAfterViewSetKey"
     )
     public static void delayAfterViewSetKey(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
-        BTraceUtils.println("get into delayAfterViewSetKey ");
-        Thread.sleep(10000L);
-    }
-
+        BTraceUtils.print("get into delayAfterViewSetKey ");
+        BTraceUtils.print(" for order __________________________ ");
+        Thread.sleep( 10000L);
+    }*/
+/*
     @OnMethod(
             clazz = "com.actiontech.dble.btrace.provider.ClusterDelayProvider",
             method = "delayAfterViewNotic"
     )
     public static void delayAfterViewNotic(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
-        BTraceUtils.println("get into delayAfterViewNotic ");
-        Thread.sleep(10000L);
-    }
-
+        BTraceUtils.print("get into delayAfterViewNotic ");
+        BTraceUtils.print(" for order __________________________ ");
+        Thread.sleep( 10000L);
+    }*/
+/*
     @OnMethod(
             clazz = "com.actiontech.dble.btrace.provider.ClusterDelayProvider",
             method = "delayWhenReponseViewNotic"
     )
     public static void delayWhenReponseViewNotic(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
-        BTraceUtils.println("get into delayWhenReponseViewNotic ");
-        Thread.sleep(10000L);
-    }
-
+        BTraceUtils.print("get into delayWhenReponseViewNotic ");
+        BTraceUtils.print(" for order __________________________ ");
+        Thread.sleep( 10000L);
+    }*/
+/*
     @OnMethod(
             clazz = "com.actiontech.dble.btrace.provider.ClusterDelayProvider",
             method = "delayBeforeReponseGetView"
     )
     public static void delayBeforeReponseGetView(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
-        BTraceUtils.println("get into delayBeforeReponseGetView ");
-        Thread.sleep(10000L);
-    }
-
+        BTraceUtils.print("get into delayBeforeReponseGetView ");
+        BTraceUtils.print(" for order __________________________ ");
+        Thread.sleep( 10000L);
+    }*/
+/*
     @OnMethod(
             clazz = "com.actiontech.dble.btrace.provider.ClusterDelayProvider",
             method = "delayBeforeReponseView"
     )
     public static void delayBeforeReponseView(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
-        BTraceUtils.println("get into delayBeforeReponseView ");
-        Thread.sleep(10000L);
-    }
-
+        BTraceUtils.print("get into delayBeforeReponseView ");
+        Thread.sleep( 10000L);
+    }*/
+/*
     @OnMethod(
             clazz = "com.actiontech.dble.btrace.provider.ClusterDelayProvider",
             method = "beforeDeleteViewNotic"
     )
     public static void beforeDeleteViewNotic(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
-        BTraceUtils.println("get into delayAfterGetLock ");
-        Thread.sleep(10000L);
-    }
-
+        BTraceUtils.print("get into delayAfterGetLock ");
+        BTraceUtils.print(" for order __________________________ ");
+        Thread.sleep( 10000L);
+    }*/
+/*
     @OnMethod(
             clazz = "com.actiontech.dble.btrace.provider.ClusterDelayProvider",
             method = "beforeReleaseViewLock"
     )
     public static void beforeReleaseViewLock(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
-        BTraceUtils.println("get into beforeReleaseViewLock ");
-        Thread.sleep(10000L);
-    }
-
+        BTraceUtils.print("get into beforeReleaseViewLock ");
+        BTraceUtils.print(" for order __________________________ ");
+        Thread.sleep( 10000L);
+    }*/
+/*
     @OnMethod(
             clazz = "com.actiontech.dble.btrace.provider.ClusterDelayProvider",
             method = "delayAfterDdlLockMeta"
     )
     public static void delayAfterDdlLockMeta(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
-        BTraceUtils.println("get into delayAfterDdlLockMeta ");
-        Thread.sleep(10000L);
-    }
-
+        BTraceUtils.print("get into delayAfterDdlLockMeta ");
+        BTraceUtils.print(" for order __________________________ ");
+        Thread.sleep( 10000L);
+    }*/
+/*
     @OnMethod(
             clazz = "com.actiontech.dble.btrace.provider.ClusterDelayProvider",
             method = "delayAfterDdlExecuted"
     )
     public static void delayAfterDdlExecuted(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
-        BTraceUtils.println("get into delayAfterDdlExecuted ");
-        Thread.sleep(10000L);
-    }
-
+        BTraceUtils.print("get into delayAfterDdlExecuted ");
+        BTraceUtils.print(" for order __________________________ ");
+        Thread.sleep( 10000L);
+    }*/
+/*
     @OnMethod(
             clazz = "com.actiontech.dble.btrace.provider.ClusterDelayProvider",
             method = "delayBeforeDdlNotice"
     )
     public static void delayBeforeDdlNotice(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
-        BTraceUtils.println("get into delayBeforeDdlNotice ");
-        Thread.sleep(10000L);
-    }
-
+        BTraceUtils.print("get into delayBeforeDdlNotice ");
+        BTraceUtils.print(" for order __________________________ ");
+        Thread.sleep( 10000L);
+    }*/
+/*
 
     @OnMethod(
             clazz = "com.actiontech.dble.btrace.provider.ClusterDelayProvider",
             method = "delayAfterDdlNotice"
     )
     public static void delayAfterDdlNotice(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
-        BTraceUtils.println("get into delayAfterDdlNotice ");
-        Thread.sleep(10000L);
-    }
-
+        BTraceUtils.print("get into delayAfterDdlNotice ");
+        BTraceUtils.print(" for order __________________________ ");
+        Thread.sleep( 10000L);
+    }*/
+/*
     @OnMethod(
             clazz = "com.actiontech.dble.btrace.provider.ClusterDelayProvider",
             method = "delayBeforeDdlNoticeDeleted"
     )
     public static void delayBeforeDdlNoticeDeleted(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
-        BTraceUtils.println("get into delayBeforeDdlNoticeDeleted ");
-        Thread.sleep(10000L);
-    }
+        BTraceUtils.print("get into delayBeforeDdlNoticeDeleted ");
+        BTraceUtils.print(" for order __________________________ ");
+        Thread.sleep( 10000L);
+    }*/
 
-
+/*
     @OnMethod(
             clazz = "com.actiontech.dble.btrace.provider.ClusterDelayProvider",
             method = "delayBeforeDdlLockRelease"
     )
     public static void delayBeforeDdlLockRelease(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
-        BTraceUtils.println("get into delayBeforeDdlLockRelease ");
-        Thread.sleep(10000L);
-    }
+        BTraceUtils.print("get into delayBeforeDdlLockRelease ");
+        BTraceUtils.print(" for order __________________________ ");
+        Thread.sleep( 10000L);
+    }*/
 
-
-    @OnMethod(
-            clazz = "com.actiontech.dble.btrace.provider.ClusterDelayProvider",
-            method = "delayAfterGetDdlNotice"
-    )
-    public static void delayAfterGetDdlNotice(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
-        BTraceUtils.println("get into delayAfterGetDdlNotice ");
-        Thread.sleep(10000L);
-    }
-
-    @OnMethod(
-            clazz = "com.actiontech.dble.btrace.provider.ClusterDelayProvider",
-            method = "delayBeforeUpdateMeta"
-    )
-    public static void delayBeforeUpdateMeta(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
-        BTraceUtils.println("get into delayBeforeUpdateMeta ");
-        Thread.sleep(10000L);
-    }
-
-
-    @OnMethod(
-            clazz = "com.actiontech.dble.btrace.provider.ClusterDelayProvider",
-            method = "delayBeforeDdlLockRelease"
-    )
-    public static void delayBeforeDdlResponse(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
-        BTraceUtils.println("get into delayBeforeDdlResponse ");
-        Thread.sleep(10000L);
-    }
 
 }

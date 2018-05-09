2e01ce6d0a2d3c65baf9b4bee3d3cc17a97bedb9
yanhuqing <yanhuqing666@gmail.com>
2018-05-09 10:01:22 +0800

    fix de9ba40e7425ca61a2b4b48e7d96c379044a4dd8

--- src/main/java/com/actiontech/dble/manager/response/ReloadConfig.java
+++ src/main/java/com/actiontech/dble/manager/response/ReloadConfig.java
@@ -43,7 +43,6 @@ import org.apache.curator.utils.ZKPaths;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;

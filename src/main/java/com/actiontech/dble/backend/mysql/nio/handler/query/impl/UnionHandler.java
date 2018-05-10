f5873b69c874a8e62003f43b8d86867262e11595
yanhuqing <yanhuqing666@gmail.com>
2018-05-10 17:10:02 +0800

    union #116 #359

--- src/main/java/com/actiontech/dble/backend/mysql/nio/handler/query/impl/UnionHandler.java
+++ src/main/java/com/actiontech/dble/backend/mysql/nio/handler/query/impl/UnionHandler.java
@@ -89,7 +89,7 @@ public class UnionHandler extends BaseDMLHandler {
             Item sel = selects.get(i);
             fp.setName(sel.getItemName().getBytes());
             fp.setDb(null);
-            fp.setTable(null);
+            fp.setTable(sel.getTableName().getBytes());
             fp.setOrgTable(null);
         }
     }

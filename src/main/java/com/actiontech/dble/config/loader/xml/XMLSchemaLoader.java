6551f7ff821d6c7dcf334674ad05df9adbfeb87a
yanhuqing <yanhuqing666@gmail.com>
2018-06-11 11:13:52 +0800

    if datahost's balance is 0, schema loader will ignore readhost #557

--- src/main/java/com/actiontech/dble/config/loader/xml/XMLSchemaLoader.java
+++ src/main/java/com/actiontech/dble/config/loader/xml/XMLSchemaLoader.java
@@ -571,7 +571,7 @@ public class XMLSchemaLoader implements SchemaLoader {
                     writeDbConfs[w] = createDBHostConf(name, writeNode, maxCon, minCon);
                     NodeList readNodes = writeNode.getElementsByTagName("readHost");
                     //for every readHost
-                    if (readNodes.getLength() != 0) {
+                    if (readNodes.getLength() != 0 && balance != 0) {
                         DBHostConfig[] readDbConfs = new DBHostConfig[readNodes.getLength()];
                         for (int r = 0; r < readDbConfs.length; r++) {
                             Element readNode = (Element) readNodes.item(r);

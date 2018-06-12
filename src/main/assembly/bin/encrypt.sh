3cca2e3b19525c4f615e97b96af98f9948b2ac16
yanhuqing <yanhuqing666@gmail.com>
2018-06-12 18:59:35 +0800

    not show password

--- src/main/assembly/bin/encrypt.sh
+++ src/main/assembly/bin/encrypt.sh
@@ -28,7 +28,9 @@ if [ -z "$APP_HOME" ] ; then
 fi 
 
 #============run encrypt
-RUN_CMD="$CORE_JAVA_HOME/bin/java -cp $APP_HOME/lib/dble*.jar com.actiontech.dble.util.DecryptUtil $@" 
-echo $RUN_CMD
+input=$1
+PASSWORD=${input/password=/""}
+RUN_CMD="$CORE_JAVA_HOME/bin/java -cp $APP_HOME/lib/dble*.jar com.actiontech.dble.util.DecryptUtil $PASSWORD"
+echo "$CORE_JAVA_HOME/bin/java -cp $APP_HOME/lib/dble*.jar com.actiontech.dble.util.DecryptUtil password=******"
 eval $RUN_CMD
 #==============================================================================

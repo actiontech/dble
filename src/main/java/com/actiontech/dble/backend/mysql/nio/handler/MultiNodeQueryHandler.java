eea3bd661089840ed8ef160a08eeee819543bad6
yanhuqing <yanhuqing666@gmail.com>
2018-06-13 15:13:08 +0800

    npe

--- src/main/java/com/actiontech/dble/backend/mysql/nio/handler/MultiNodeQueryHandler.java
+++ src/main/java/com/actiontech/dble/backend/mysql/nio/handler/MultiNodeQueryHandler.java
@@ -183,6 +183,10 @@ public class MultiNodeQueryHandler extends MultiNodeHandler implements LoadDataR
         try {
             if (!isFail()) {
                 setFail(new String(err.getMessage()));
+                if (errConnection == null) {
+                    errConnection = new ArrayList<>();
+                }
+                errConnection.add(conn);
             }
             if (!conn.syncAndExecute()) {
                 return;

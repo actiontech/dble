7d147ee92e63723982a09b725fb122da2fdbbba7
sunzhengfang <sunzhengfang@actionsky.com>
2018-07-25 10:54:15 +0800

    add btrace script

---
+++ src/main/assembly/bin/btrace_log.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+APP_NAME='dble'
+CORE_JAVA_HOME=$JAVA_HOME
+#check CORE_JAVA_HOME & java
+noJavaHome=false
+if [ -z "$CORE_JAVA_HOME" ] ; then
+    noJavaHome=true
+fi
+if [ ! -e "$CORE_JAVA_HOME/bin/java" ] ; then
+    noJavaHome=true
+fi
+if $noJavaHome ; then
+    echo
+    echo "Error: CORE_JAVA_HOME environment variable is not set."
+    echo
+    exit 1
+fi
+
+input=$1
+export JAVA_HOME=$CORE_JAVA_HOME
+BASEDIR=`dirname "$0"`/../..
+BASEDIR=`(cd "$BASEDIR"; pwd)`
+pid=`(ps -ef | grep "lib/$APP_NAME" | grep -v grep|awk '{print $2}')`
+echo "su  actiontech-ushard -s /bin/bash timeout $1   $BASEDIR/btrace/bin/btrace $pid $BASEDIR/btrace/bin/BTraceCostTime.java > /$BASEDIR/core/traceCost.log"
+timeout $1 su actiontech-ushard -s /bin/bash $BASEDIR/btrace/bin/btrace $pid $BASEDIR/btrace/bin/BTraceCostTime.java > /$BASEDIR/core/traceCost.log

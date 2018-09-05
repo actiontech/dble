1cf8104eb9749520c14fddee3c7b5219934baa31
yanhuqing <yanhuqing666@gmail.com>
2018-09-05 15:46:47 +0800

    derived subquery is global table #685

--- src/main/java/com/actiontech/dble/backend/mysql/nio/handler/builder/BaseHandlerBuilder.java
+++ src/main/java/com/actiontech/dble/backend/mysql/nio/handler/builder/BaseHandlerBuilder.java
@@ -97,7 +97,7 @@ public abstract class BaseHandlerBuilder {
         boolean isNestLoopJoin = isNestLoopStrategy(node);
         if (isNestLoopJoin) {
             nestLoopBuild();
-        } else if (!node.isExistView() && PlanUtil.isGlobal(node)) {
+        } else if (PlanUtil.isGlobal(node)) {
             // the query can be send to a certain node
             noShardBuild();
         } else if (canDoAsMerge()) {

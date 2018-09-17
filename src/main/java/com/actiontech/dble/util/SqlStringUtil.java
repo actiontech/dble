16294f409ed58f5fc25f8db508c28020e1835bf1
sunzhengfang <sunzhengfang@actionsky.com>
2018-09-17 16:02:15 +0800

    #702 load data do not send the character to backend

---
+++ src/main/java/com/actiontech/dble/util/SqlStringUtil.java
@@ -0,0 +1,28 @@
+package com.actiontech.dble.util;
+
+import com.actiontech.dble.server.parser.DbleOutputVisitor;
+import com.alibaba.druid.sql.SQLUtils;
+import com.alibaba.druid.sql.ast.SQLObject;
+import com.alibaba.druid.sql.visitor.SQLASTOutputVisitor;
+
+/**
+ * Created by szf on 2018/9/17.
+ */
+public final class SqlStringUtil {
+
+    private SqlStringUtil() {
+
+    }
+
+    public static String toSQLString(SQLObject sqlObject) {
+        StringBuilder out = new StringBuilder();
+        SQLASTOutputVisitor visitor = new DbleOutputVisitor(out);
+        SQLUtils.FormatOption option = new SQLUtils.FormatOption(true, true);
+        visitor.setUppCase(option.isUppCase());
+        visitor.setPrettyFormat(option.isPrettyFormat());
+        visitor.setParameterized(option.isParameterized());
+        sqlObject.accept(visitor);
+        String sql = out.toString();
+        return sql;
+    }
+}

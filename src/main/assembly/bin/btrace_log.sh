e788790ba72e66dc8d2da157e68d367d2c1933b9
zhengfang.sun <sunsun3141@gmail.com>
2018-09-19 17:41:13 +0800

    trace script update (#709)

--- src/main/assembly/bin/btrace_log.sh
+++ src/main/assembly/bin/btrace_log.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 APP_NAME='dble'
-CORE_JAVA_HOME=$JAVA_HOME
+basepath=$(cd `dirname $0`; pwd)/../..
+basepath=`(cd "$basepath"; pwd)`
 
 if [ "$1" -gt 0 ] 2>/dev/null ;then
     echo "recording btrace log for $1 seconds"
@@ -9,26 +10,8 @@ else
     exit 1
 fi
 
-
-#check CORE_JAVA_HOME & java
-noJavaHome=false
-if [ -z "$CORE_JAVA_HOME" ] ; then
-    noJavaHome=true
-fi
-if [ ! -e "$CORE_JAVA_HOME/bin/java" ] ; then
-    noJavaHome=true
-fi
-if $noJavaHome ; then
-    echo
-    echo "Error: CORE_JAVA_HOME environment variable is not set."
-    echo
-    exit 1
-fi
-
 input=$1
-export JAVA_HOME=$CORE_JAVA_HOME
-BASEDIR=`dirname "$0"`/../..
-BASEDIR=`(cd "$BASEDIR"; pwd)`
+export JAVA_HOME=$basepath/jdk
 pid=`(ps -ef | grep "lib/$APP_NAME" | grep -v grep|awk '{print $2}')`
-echo "su  actiontech-ushard -s /bin/bash timeout $1   $BASEDIR/btrace/bin/btrace $pid $BASEDIR/btrace/bin/BTraceCostTime.java > /$BASEDIR/core/traceCost.log"
-timeout $1 su actiontech-ushard -s /bin/bash $BASEDIR/btrace/bin/btrace $pid $BASEDIR/btrace/bin/BTraceCostTime.java > /$BASEDIR/core/traceCost.log
+echo "su  actiontech-ushard -s /bin/bash timeout $1   $basepath/btrace/bin/btrace $pid $basepath/btrace/bin/BTraceCostTime.java > /$basepath/core/traceCost.log"
+timeout $1 su actiontech-ushard -s /bin/bash $basepath/btrace/bin/btrace $pid $basepath/btrace/bin/BTraceCostTime.java > /$basepath/core/traceCost.log

1338886e633dd68cc4a21dbd410cfacef238fe48
sunzhengfang <sunzhengfang@actionsky.com>
2018-11-05 14:43:26 +0800

    #797 Close the session while doing DDL, the DDL lock may not be removed

--- src/main/java/com/actiontech/dble/server/NonBlockingSession.java
+++ src/main/java/com/actiontech/dble/server/NonBlockingSession.java
@@ -64,6 +64,7 @@ import java.util.concurrent.locks.LockSupport;
 
 import static com.actiontech.dble.meta.PauseEndThreadPool.CONTINUE_TYPE_MULTIPLE;
 import static com.actiontech.dble.meta.PauseEndThreadPool.CONTINUE_TYPE_SINGLE;
+import static com.actiontech.dble.server.parser.ServerParse.DDL;
 
 /**
  * @author mycat
@@ -867,6 +868,13 @@ public class NonBlockingSession implements Session {
         clearHandlesResources();
         source.setTxStart(false);
         source.getAndIncrementXid();
+    }
+
+    public void clearResources(RouteResultset rrs) {
+        clearResources(true);
+        if (rrs.getSqlType() == DDL) {
+            DbleServer.getInstance().getTmManager().removeMetaLock(rrs.getSchema(), rrs.getTable());
+        }
     }
 
     public boolean closed() {

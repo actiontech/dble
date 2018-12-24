ecbab407988ca56ebd674295896659eeee4b08cc
zhengfang.sun <sunsun3141@gmail.com>
2018-12-24 10:10:13 +0800

    #899 get detail error when explain sql can not be parsed by druid (#906)
    * #899 get detail error when explain sql can not be parsed by druid
    
    * #899 get detail error when explain sql can not be parsed by druid

--- src/main/java/com/actiontech/dble/server/handler/ExplainHandler.java
+++ src/main/java/com/actiontech/dble/server/handler/ExplainHandler.java
@@ -40,6 +40,7 @@ import org.slf4j.LoggerFactory;
 import java.nio.ByteBuffer;
 import java.sql.SQLException;
 import java.sql.SQLNonTransientException;
+import java.sql.SQLSyntaxErrorException;
 import java.util.List;
 
 /**
@@ -198,6 +199,12 @@ public final class ExplainHandler {
                 LOGGER.info(s.append(c).append(stmt).toString() + " error:" + sqlException);
                 String msg = sqlException.getMessage();
                 c.writeErrMessage(sqlException.getErrorCode(), msg == null ? sqlException.getClass().getSimpleName() : msg);
+                return null;
+            } else if (e instanceof SQLSyntaxErrorException) {
+                StringBuilder s = new StringBuilder();
+                LOGGER.info(s.append(c).append(stmt).toString() + " error:" + e);
+                String msg = "druid parse sql error:" + e.getMessage();
+                c.writeErrMessage(ErrorCode.ER_PARSE_ERROR, msg);
                 return null;
             } else {
                 StringBuilder s = new StringBuilder();

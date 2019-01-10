dfcc823eb6595f8e98bb78619a225352c5e31f8d
sunzhengfang <sunzhengfang@actionsky.com>
2019-01-10 18:13:09 +0800

    #add dockerfile for quick start

---
+++ docker-images/conf/docker_init_start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "dble init&start in docker"
+
+sh /opt/dble/bin/dble start
+sleep 20
+mysql -P9066 -u man1 -h 127.0.0.1 -p654321 -e "create database @@dataNode ='dn1,dn2,dn3,dn4'"
+mysql -P8066 -u root -h 127.0.0.1 -p123456 -e "source /opt/dble/conf/testdb.sql" testdb
+
+echo "dble init finish"
+
+/bin/bash

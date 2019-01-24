66ff63a4377428e48842cb16e1662e40e315c1b4
yanhuqing <yanhuqing666@gmail.com>
2019-01-24 15:46:47 +0800

    docker wait_server start

--- docker-images/quick-start/docker_init_start.sh
+++ docker-images/quick-start/docker_init_start.sh
@@ -3,7 +3,7 @@
 echo "dble init&start in docker"
 
 sh /opt/dble/bin/dble start
-sleep 20
+sh /opt/dble/bin/wait-for-it.sh 127.0.0.1:8066
 mysql -P9066 -u man1 -h 127.0.0.1 -p654321 -e "create database @@dataNode ='dn1,dn2,dn3,dn4'"
 mysql -P8066 -u root -h 127.0.0.1 -p123456 -e "source /opt/dble/conf/testdb.sql" testdb
 

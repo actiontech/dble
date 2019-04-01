a81c0b12e33862858cf4b0ff532ff849bbcee59c
collapsar <lpxlbfq931126>
2019-04-01 18:02:13 +0800

    #1078 - improve dble config template

--- src/main/resources/testdb.sql
+++ src/main/resources/testdb.sql
@@ -1,96 +1,130 @@
 -- testdb schema for test only
--- @since 2018-09-27
--- @author little-pan
+-- @since 2019-04-01
+-- @author collapsar
 --
 
--- auto sharding by id (long)
-create table if not exists travelrecord (
-  id bigint not null auto_increment,
-  participants varchar(250) not null,
-  origin varchar(50) not null,
-  destination varchar(50) not null,
-  start_time datetime not null,
-  end_time datetime not null,
-  description text,
-  primary key(id)
-)engine=innodb charset=utf8;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- global tables
-create table if not exists company (
-  id bigint not null auto_increment,
-  name varchar(20) not null,
-  address varchar(250) not null,
-  create_time timestamp not null,
-  owner varchar(20) not null,
-  employees integer not null default 50,
-  primary key(id)
-)engine=innodb charset=utf8;
+--
+-- Table structure for table `tb_goods`
+--
 
-create table if not exists goods (
-  id bigint not null auto_increment,
-  name varchar(50) not null,
-  price decimal(9,2) not null,
-  quantity integer not null,
-  create_time timestamp not null,
-  images varchar(250),
-  primary key(id)
-)engine=innodb charset=utf8;
+DROP TABLE IF EXISTS `tb_goods`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_goods` (
+  `id` bigint(20) NOT NULL COMMENT '商品id',
+  `sn` varchar(120) DEFAULT NULL COMMENT '商品编号',
+  `name` varchar(120) DEFAULT NULL COMMENT '商品名称',
+  `store_count` smallint(5) unsigned DEFAULT '10' COMMENT '库存数量',
+  `price` decimal(10,2) DEFAULT '0.00' COMMENT '商品单价',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- random sharding
-create table if not exists hotnews (
-  id bigint not null auto_increment,
-  title varchar(50) not null,
-  create_time datetime not null,
-  author varchar(20) null,
-  clicks integer not null default 0,
-  content text,
-  primary key(id)
-)engine=innodb charset=utf8;
+--
+-- Dumping data for table `tb_goods`
+--
 
--- ER sharding
-create table if not exists customer (
-  id bigint not null auto_increment,
-  name varchar(20) not null,
-  birth_date date null,
-  birth_day time null,
-  height double null,
-  weight double null,
-  contact varchar(50) null,
-  address varchar(80) null,
-  hobby varchar(250) null,
-  primary key(id)
-)engine=innodb charset=utf8;
--- issue
--- mysql> explain select*from customer where id=2;
--- ERROR 1064 (HY000): Can't find a valid data node for specified node index :customer -> ID -> 2 -> Index : 2
+LOCK TABLES `tb_goods` WRITE;
+/*!40000 ALTER TABLE `tb_goods` DISABLE KEYS */;
+INSERT INTO `tb_goods` VALUES (2,'100001860765','AppleiPhone Xs Max',100,7999.00),(4,'1385736','欧德堡牛奶',5,79.00),(1,'1603401883','吉普JEEP牛仔裤男士',100,148.00),(3,'6072622','ThinkPad翼480',50,5399.00);
+/*!40000 ALTER TABLE `tb_goods` ENABLE KEYS */;
+UNLOCK TABLES;
 
-create table if not exists orders (
-  id bigint not null auto_increment,
-  order_no varchar(60) not null,
-  order_time datetime not null,
-  amount decimal(12, 2) not null,
-  customer_id bigint not null,
-  primary key(id),
-  foreign key(customer_id) references customer(id)
-)engine=innodb charset=utf8;
+--
+-- Table structure for table `tb_order`
+--
 
-create table if not exists order_items (
-  id bigint not null auto_increment,
-  goods_id bigint not null,
-  goods_name varchar(50) not null,
-  price decimal(9, 2) not null,
-  quantity integer not null,
-  order_id bigint not null,
-  primary key(id),
-  foreign key(goods_id) references goods(id),
-  foreign key(order_id) references orders(id)
-)engine=innodb charset=utf8;
+DROP TABLE IF EXISTS `tb_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_order` (
+  `id` bigint(20) NOT NULL COMMENT '订单id',
+  `code` varchar(25) DEFAULT NULL COMMENT '订单号',
+  `user_id` bigint(20) DEFAULT NULL COMMENT '用户id',
+  `amount` decimal(10,2) DEFAULT '0.00' COMMENT '付款金额',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table if not exists customer_addr (
-  id bigint not null auto_increment,
-  address varchar(80) not null,
-  contact varchar(50) not null,
-  customer_id bigint not null,
-  primary key(id),
-  foreign key(customer_id) references customer(id)
-)engine=innodb charset=utf8;
+--
+-- Dumping data for table `tb_order`
+--
+
+LOCK TABLES `tb_order` WRITE;
+/*!40000 ALTER TABLE `tb_order` DISABLE KEYS */;
+INSERT INTO `tb_order` VALUES (1,'20181027160000000001',1,16294.00,'2019-04-01 09:38:56'),(3,'20181027160000000003',3,237.00,'2019-04-01 09:38:56'),(4,'20181027160000000004',4,31196.00,'2019-04-01 09:38:56'),(2,'20181027160000000002',2,26995.00,'2019-04-01 09:38:56');
+/*!40000 ALTER TABLE `tb_order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_order_item`
+--
+
+DROP TABLE IF EXISTS `tb_order_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_order_item` (
+  `order_id` bigint(20) NOT NULL COMMENT '订单id',
+  `goods_id` bigint(20) NOT NULL COMMENT '商品id',
+  `goods_num` bigint(20) DEFAULT '0' COMMENT '购买数量',
+  `price` decimal(10,2) DEFAULT '0.00' COMMENT '商品单价'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_order_item`
+--
+
+LOCK TABLES `tb_order_item` WRITE;
+/*!40000 ALTER TABLE `tb_order_item` DISABLE KEYS */;
+INSERT INTO `tb_order_item` VALUES (1,1,2,148.00),(1,2,2,7999.00),(3,4,3,79.00),(4,2,4,7999.00),(2,3,5,5399.00);
+/*!40000 ALTER TABLE `tb_order_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_user`
+--
+
+DROP TABLE IF EXISTS `tb_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_user` (
+  `id` bigint(20) NOT NULL COMMENT '用户id',
+  `username` varchar(20) DEFAULT NULL COMMENT '用户名称',
+  `age` tinyint(4) DEFAULT NULL COMMENT '用户年龄',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+LOCK TABLES `tb_user` WRITE;
+/*!40000 ALTER TABLE `tb_user` DISABLE KEYS */;
+INSERT INTO `tb_user` VALUES (1,'zhangsan',18),(2,'lisi',19),(3,'zhanglong',20),(4,'zhaohu',28);
+/*!40000 ALTER TABLE `tb_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-04-01 17:57:00

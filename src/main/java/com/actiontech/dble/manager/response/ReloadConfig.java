d315da93cfb4d4520b189e0cb7878d22555192d1
yanhuqing <yanhuqing666@gmail.com>
2019-05-28 15:14:08 +0800

    tips of reload config

--- src/main/java/com/actiontech/dble/manager/response/ReloadConfig.java
+++ src/main/java/com/actiontech/dble/manager/response/ReloadConfig.java
@@ -429,16 +429,18 @@ public final class ReloadConfig {
                     for (BackendConnection con : processor.getBackends().values()) {
                         if (con instanceof MySQLConnection) {
                             MySQLConnection mysqlCon = (MySQLConnection) con;
-                            LOGGER.info("mysqlCon.getPool() == " + mysqlCon.getPool() + " " + mysqlCon.getSchema() + " " + mysqlCon.getId());
                             if (mysqlCon.getPool() == ds) {
                                 if (con.isBorrowed()) {
                                     if (closeFrontCon) {
+                                        LOGGER.info("old active backend conn will be forced closed by closing front conn, conn info:" + mysqlCon);
                                         findAndcloseFrontCon(con);
                                     } else {
+                                        LOGGER.info("old active backend conn will be added to old pool, conn info:" + mysqlCon);
                                         con.setOldTimestamp(oldTimestamp);
                                         NIOProcessor.BACKENDS_OLD.add(con);
                                     }
                                 } else {
+                                    LOGGER.info("old idle backend conn will be closed, conn info:" + mysqlCon);
                                     con.close("old idle conn for reload merge");
                                 }
                             }

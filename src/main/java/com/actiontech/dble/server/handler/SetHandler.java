4233fd94c10ee6c5854923ca509530ec717cef06
yanhuqing <yanhuqing666@gmail.com>
2019-06-26 18:02:58 +0800

    default conn #1245

--- src/main/java/com/actiontech/dble/server/handler/SetHandler.java
+++ src/main/java/com/actiontech/dble/server/handler/SetHandler.java
@@ -6,7 +6,6 @@
 package com.actiontech.dble.server.handler;
 
 import com.actiontech.dble.DbleServer;
-import com.actiontech.dble.backend.datasource.PhysicalDBPool;
 import com.actiontech.dble.backend.mysql.CharsetUtil;
 import com.actiontech.dble.config.ErrorCode;
 import com.actiontech.dble.config.Isolations;
@@ -31,7 +30,6 @@ import com.alibaba.druid.sql.parser.SQLStatementParser;
 
 import java.sql.SQLSyntaxErrorException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -192,14 +190,8 @@ public final class SetHandler {
     private static void setStmtCallback(String multiStmt, ServerConnection c, List<Pair<KeyType, Pair<String, String>>> contextTask) {
         c.setContextTask(contextTask);
         OneRawSQLQueryResultHandler resultHandler = new OneRawSQLQueryResultHandler(new String[0], new SetCallBack(c));
-        Iterator<PhysicalDBPool> iterator = DbleServer.getInstance().getConfig().getDataHosts().values().iterator();
-        if (iterator.hasNext()) {
-            PhysicalDBPool pool = iterator.next();
-            SetTestJob sqlJob = new SetTestJob(multiStmt, pool.getSchemas()[0], resultHandler, c);
-            sqlJob.run();
-        } else {
-            c.writeErrMessage(ErrorCode.ER_YES, "no valid data host");
-        }
+        SetTestJob sqlJob = new SetTestJob(multiStmt, null, resultHandler, c);
+        sqlJob.run();
     }
 
     private static boolean handleVariableInMultiStmt(SQLAssignItem assignItem, ServerConnection c, List<Pair<KeyType, Pair<String, String>>> contextTask) {

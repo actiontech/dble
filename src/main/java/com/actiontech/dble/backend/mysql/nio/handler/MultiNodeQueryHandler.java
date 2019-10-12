a65eac153268517e80bdb960d5e6d9f93105348f
yanhuqing <yanhuqing666@gmail.com>
2019-10-12 19:01:49 +0800

    commit all closed conns

--- src/main/java/com/actiontech/dble/backend/mysql/nio/handler/MultiNodeQueryHandler.java
+++ src/main/java/com/actiontech/dble/backend/mysql/nio/handler/MultiNodeQueryHandler.java
@@ -494,6 +494,13 @@ public class MultiNodeQueryHandler extends MultiNodeHandler implements LoadDataR
                 err.setPacketId(++packetId);
                 handleEndPacket(err.toBytes(), AutoTxOperation.ROLLBACK, conn, false);
             }
+        } else if (errConnection.size() == rrs.getNodes().length && (!session.getSource().isAutocommit() || session.getSource().isTxStart())) {
+            for (BackendConnection errConn : errConnection) {
+                if (!errConn.isClosed()) {
+                    return;
+                }
+            }
+            session.getSource().setTxInterrupt(error);
         }
     }
 

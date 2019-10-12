161ba1d05a0ecca0633ca0f463704149b16fdef7
yanhuqing <yanhuqing666@gmail.com>
2019-10-12 20:34:27 +0800

    when close in trx,then need rollback

--- src/main/java/com/actiontech/dble/backend/mysql/nio/handler/MultiNodeQueryHandler.java
+++ src/main/java/com/actiontech/dble/backend/mysql/nio/handler/MultiNodeQueryHandler.java
@@ -485,6 +485,9 @@ public class MultiNodeQueryHandler extends MultiNodeHandler implements LoadDataR
             errConnection = new ArrayList<>();
         }
         errConnection.add(conn);
+        if (conn.isClosed() && (!session.getSource().isAutocommit() || session.getSource().isTxStart())) {
+            session.getSource().setTxInterrupt(error);
+        }
         if (canResponse()) {
             session.handleSpecial(rrs, false);
             if (byteBuffer == null) {
@@ -494,13 +497,6 @@ public class MultiNodeQueryHandler extends MultiNodeHandler implements LoadDataR
                 err.setPacketId(++packetId);
                 handleEndPacket(err.toBytes(), AutoTxOperation.ROLLBACK, conn, false);
             }
-        } else if (errConnection.size() == rrs.getNodes().length && (!session.getSource().isAutocommit() || session.getSource().isTxStart())) {
-            for (BackendConnection errConn : errConnection) {
-                if (!errConn.isClosed()) {
-                    return;
-                }
-            }
-            session.getSource().setTxInterrupt(error);
         }
     }
 

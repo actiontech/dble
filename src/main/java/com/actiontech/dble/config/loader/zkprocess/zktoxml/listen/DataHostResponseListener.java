3f0abefb6ed25d42f007af12f3c20fc4853475b6
sunzhengfang <sunzhengfang@actionsky.com>
2019-11-07 19:21:10 +0800

    #1467 avoid reponse to self

--- src/main/java/com/actiontech/dble/config/loader/zkprocess/zktoxml/listen/DataHostResponseListener.java
+++ src/main/java/com/actiontech/dble/config/loader/zkprocess/zktoxml/listen/DataHostResponseListener.java
@@ -6,6 +6,7 @@ import com.actiontech.dble.cluster.ClusterParamCfg;
 import com.actiontech.dble.cluster.ClusterPathUtil;
 import com.actiontech.dble.config.loader.zkprocess.comm.ZkConfig;
 import com.actiontech.dble.config.loader.zkprocess.zookeeper.process.HaInfo;
+import com.actiontech.dble.singleton.ClusterGeneralConfig;
 import com.actiontech.dble.singleton.HaConfigManager;
 import com.actiontech.dble.util.KVPathUtil;
 import com.actiontech.dble.util.ZKUtils;
@@ -18,7 +19,6 @@ import org.slf4j.LoggerFactory;
 
 import java.nio.charset.StandardCharsets;
 
-import static com.actiontech.dble.util.KVPathUtil.SEPARATOR;
 
 /**
  * Created by szf on 2019/10/30.
@@ -45,22 +45,35 @@ public class DataHostResponseListener implements PathChildrenCacheListener {
 
 
     private void updateStatus(ChildData childData) throws Exception {
+        String data = new String(childData.getData(), StandardCharsets.UTF_8);
+        LOGGER.info("Ha disable node " + childData.getPath() + " updated , and data is " + data);
         try {
-            String data = new String(childData.getData(), StandardCharsets.UTF_8);
-            LOGGER.info("Ha disable node " + childData.getPath() + " updated , and data is " + data);
-            CuratorFramework zkConn = ZKUtils.getConnection();
-            String[] pathSplit = childData.getPath().split(SEPARATOR);
-            String dhName = pathSplit[pathSplit.length - 1];
-            int id = HaConfigManager.getInstance().haStart(HaInfo.HaStage.RESPONSE_NOTIFY, HaInfo.HaStartType.CLUSTER_NOTIFY, HaInfo.HaStage.RESPONSE_NOTIFY.toString());
-            PhysicalDNPoolSingleWH dataHost = (PhysicalDNPoolSingleWH) DbleServer.getInstance().getConfig().getDataHosts().get(dhName);
-            String jsonString = new String(zkConn.getData().forPath(KVPathUtil.getHaStatusPath(dhName)), "UTF-8");
-            dataHost.changeIntoLatestStatus(jsonString);
-            //response to kv
-            ZKUtils.createTempNode(childData.getPath(), ZkConfig.getInstance().getValue(ClusterParamCfg.CLUSTER_CFG_MYID), ClusterPathUtil.SUCCESS.getBytes());
-            HaConfigManager.getInstance().haFinish(id, null, data);
+            if (data != null && !"".equals(data)) {
+                response(data, childData.getPath());
+            } else {
+                CuratorFramework zkConn = ZKUtils.getConnection();
+                String newData = new String(zkConn.getData().forPath(childData.getPath()), "UTF-8");
+                response(newData, childData.getPath());
+            }
         } catch (Exception e) {
             LOGGER.warn("get error when try to response to the disable");
             ZKUtils.createTempNode(childData.getPath(), ZkConfig.getInstance().getValue(ClusterParamCfg.CLUSTER_CFG_MYID), e.getMessage().getBytes());
+        }
+    }
+
+
+    private void response(String data, String path) throws Exception {
+        HaInfo info = new HaInfo(data);
+        CuratorFramework zkConn = ZKUtils.getConnection();
+        if (!info.getStartId().equals(ClusterGeneralConfig.getInstance().getValue(ClusterParamCfg.CLUSTER_CFG_MYID)) &&
+                info.getStatus() == HaInfo.HaStatus.SUCCESS) {
+            int id = HaConfigManager.getInstance().haStart(HaInfo.HaStage.RESPONSE_NOTIFY, HaInfo.HaStartType.CLUSTER_NOTIFY, HaInfo.HaStage.RESPONSE_NOTIFY.toString());
+            PhysicalDNPoolSingleWH dataHost = (PhysicalDNPoolSingleWH) DbleServer.getInstance().getConfig().getDataHosts().get(info.getDhName());
+            String jsonString = new String(zkConn.getData().forPath(KVPathUtil.getHaStatusPath(info.getDhName())), "UTF-8");
+            dataHost.changeIntoLatestStatus(jsonString);
+            //response to kv
+            ZKUtils.createTempNode(path, ZkConfig.getInstance().getValue(ClusterParamCfg.CLUSTER_CFG_MYID), ClusterPathUtil.SUCCESS.getBytes());
+            HaConfigManager.getInstance().haFinish(id, null, data);
         }
     }
 

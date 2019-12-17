7614e172184e1efc1977e469e4f99b4a786579be
sunzhengfang <sunzhengfang@actionsky.com>
2019-12-17 10:58:22 +0800

    #1526 code style

--- src/main/java/com/actiontech/dble/config/loader/xml/XMLSchemaLoader.java
+++ src/main/java/com/actiontech/dble/config/loader/xml/XMLSchemaLoader.java
@@ -18,8 +18,6 @@ import com.actiontech.dble.route.function.AbstractPartitionAlgorithm;
 import com.actiontech.dble.util.DecryptUtil;
 import com.actiontech.dble.util.ResourceUtil;
 import com.actiontech.dble.util.SplitUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;

3f3bd0bc7dd70967937467010a3fac68ab70a49d
yanhuqing <yanhuqing666@gmail.com>
2019-12-19 16:29:50 +0800

    use memory disable

--- src/main/java/com/actiontech/dble/backend/datasource/PhysicalDBNode.java
+++ src/main/java/com/actiontech/dble/backend/datasource/PhysicalDBNode.java
@@ -138,7 +138,7 @@ public class PhysicalDBNode {
         checkRequest(schema);
         if (dbPool.isInitSuccess()) {
             PhysicalDatasource writeSource = dbPool.getSource();
-            if (writeSource.getConfig().isDisabled()) {
+            if (writeSource.isDisabled()) {
                 throw new IllegalArgumentException("[" + writeSource.getHostConfig().getName() + "." + writeSource.getConfig().getHostName() + "] is disabled");
             }
 

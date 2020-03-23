386b60829199a0d8e508e79d26dfeeea50d9aa78
tiger.yan <yanhuqing666@gmail.com>
2020-03-23 10:03:13 +0800

    #1682 heartbeat deadlock (#1700)

--- src/main/java/com/actiontech/dble/backend/datasource/PhysicalDatasource.java
+++ src/main/java/com/actiontech/dble/backend/datasource/PhysicalDatasource.java
@@ -338,9 +338,7 @@ public abstract class PhysicalDatasource {
         if (TimeUtil.currentTimeMillis() < heartbeatRecoveryTime) {
             return;
         }
-        if (!heartbeat.isStop()) {
-            heartbeat.heartbeat();
-        }
+        heartbeat.heartbeat();
     }
 
     private BackendConnection takeCon(BackendConnection conn, String schema) {

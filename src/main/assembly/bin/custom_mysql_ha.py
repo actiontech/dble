d0cf5471d3cc90b6d49bbc812d7cc760a34be7f3
zhouqichao <zqcn5044@126.com>
2020-04-29 12:52:17 +0800

    修改了dble_datahosts_check.py文件isAlive(datahost)函数
    修改了dble_datahosts_check.py文件 isAlive(datahost)函数。添加了版本检查。

---
+++ src/main/assembly/bin/custom_mysql_ha.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import time
+import logging.config
+import dble_datahosts_check as Dhcheck
+
+# Need to update.
+
+def init():
+
+    # logfile for initialization
+
+    global logfile
+    logfile = './bin/custom_mysql_ha_logging.conf'
+    global loggername
+    loggername = 'DBLEDatahostCheck'
+
+    # dble schema.xml.
+
+    global schemaxml
+    schemaxml = './conf/schema.xml'
+
+    # dble manage user.
+
+    global serverxml
+    serverxml = './conf/server.xml'
+
+# Log file initialization.
+
+def logInit(logfile,loggername):
+    logging.config.fileConfig(logfile)
+    log = logging.getLogger(name=loggername)
+    return log
+
+if __name__ == "__main__":
+
+    # Parameters initialization.
+
+    init()
+
+    log = logInit(logfile,loggername)
+    log.info("Logger initialization is complete.")
+    while "true":
+        log.info("DBLE datahsots check begin...")
+        Dhcheck.main(log,schemaxml,serverxml)
+        log.info("DBLE datahsots check end.")
+        time.sleep(5)
+
+

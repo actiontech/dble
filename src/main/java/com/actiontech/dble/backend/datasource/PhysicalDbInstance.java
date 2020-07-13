5adb14dbcd5c280d45dbe8ebceab3817d33becd2
Collapsar <838800176@qq.com>
2020-07-13 09:56:46 +0800

    Fix some bug about connection pool (#1935)
    * fix bug about connection pool
    
    * fix bug about connection pool

--- src/main/java/com/actiontech/dble/backend/datasource/PhysicalDbInstance.java
+++ src/main/java/com/actiontech/dble/backend/datasource/PhysicalDbInstance.java
@@ -53,6 +53,7 @@ public abstract class PhysicalDbInstance {
     private ConnectionPool connectionPool;
     protected MySQLHeartbeat heartbeat;
     private volatile long heartbeatRecoveryTime;
+    private volatile boolean needSkipEvit = false;
 
 
     public PhysicalDbInstance(DbInstanceConfig config, DbGroupConfig dbGroupConfig, boolean isReadNode) {
@@ -347,16 +348,23 @@ public abstract class PhysicalDbInstance {
     }
 
     public void stop(String reason, boolean closeFront) {
+        LOGGER.info("stop connection pool of physical db instance[{}], due to {}", name, reason);
         heartbeat.stop(reason);
         connectionPool.stop(reason, closeFront);
     }
 
     public void closeAllConnection(String reason) {
+        this.needSkipEvit = true;
         this.connectionPool.closeAllConnections(reason);
+        this.needSkipEvit = false;
     }
 
     public boolean isAlive() {
         return !disabled.get() && !isFakeNode() && heartbeat.isHeartBeatOK();
+    }
+
+    public boolean skipEvit() {
+        return !isAlive() && needSkipEvit;
     }
 
     public boolean isDisabled() {

7d0716ff6e365969b33d8c168f288f1a1b72bf45
sunsun314 <sunsun3141@gmail.com>
2020-08-07 14:20:53 +0800

    fix NP in manager complex select

--- src/main/java/com/actiontech/dble/backend/mysql/nio/handler/query/impl/OrderByHandler.java
+++ src/main/java/com/actiontech/dble/backend/mysql/nio/handler/query/impl/OrderByHandler.java
@@ -59,7 +59,9 @@ public class OrderByHandler extends OwnThreadDMLHandler {
 
         this.fieldPackets = fieldPackets;
         RowDataComparator cmp = new RowDataComparator(this.fieldPackets, orders, isAllPushDown(), type());
-        localResult = new SortedLocalResult(pool, fieldPackets.size(), cmp, CharsetUtil.getJavaCharset(((MySQLResponseService) service).getCharset().getResults())).
+        String charSet = service != null ? CharsetUtil.getJavaCharset(service.getConnection().getCharsetName().getResults()) :
+                CharsetUtil.getJavaCharset(session.getSource().getService().getConnection().getCharsetName().getResults());
+        localResult = new SortedLocalResult(pool, fieldPackets.size(), cmp, charSet).
                 setMemSizeController(session.getOrderBufferMC());
         nextHandler.fieldEofResponse(null, null, fieldPackets, null, this.isLeft, service);
         startOwnThread(service);

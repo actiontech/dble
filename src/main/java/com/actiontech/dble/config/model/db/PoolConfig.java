8fe82747e302bc32af85f4f94dd52a4408b9c76b
LUA <guoaomen@actionsky.com>
2020-09-21 13:05:51 +0800

    fix: inner 539/540/541 (#2133)
    * fix: inner 539/540/541
    
    * fix: update default value

--- src/main/java/com/actiontech/dble/config/model/db/PoolConfig.java
+++ src/main/java/com/actiontech/dble/config/model/db/PoolConfig.java
@@ -10,11 +10,12 @@ public class PoolConfig {
     private static final Logger LOGGER = LoggerFactory.getLogger(PoolConfig.class);
     private static final String WARNING_FORMAT = "Property [ %s ] '%d' in db.xml is illegal, use the default value %d replaced";
 
-    private static final long CONNECTION_TIMEOUT = SECONDS.toMillis(30);
-    private static final long CON_HEARTBEAT_TIMEOUT = MILLISECONDS.toMillis(20);
+    public static final long CONNECTION_TIMEOUT = SECONDS.toMillis(30);
+    public static final long CON_HEARTBEAT_TIMEOUT = MILLISECONDS.toMillis(20);
     public static final long DEFAULT_IDLE_TIMEOUT = MINUTES.toMillis(10);
-    private static final long HOUSEKEEPING_PERIOD_MS = SECONDS.toMillis(30);
-    private static final long DEFAULT_HEARTBEAT_PERIOD = SECONDS.toMillis(10);
+    public static final long HOUSEKEEPING_PERIOD_MS = SECONDS.toMillis(30);
+    public static final long DEFAULT_HEARTBEAT_PERIOD = SECONDS.toMillis(10);
+    public static final long DEFAULT_SHUTDOWN_TIMEOUT = SECONDS.toMillis(10);
 
     private volatile long connectionTimeout = CONNECTION_TIMEOUT;
     private volatile long connectionHeartbeatTimeout = CON_HEARTBEAT_TIMEOUT;
@@ -24,7 +25,7 @@ public class PoolConfig {
     private volatile boolean testWhileIdle = false;
     private volatile long timeBetweenEvictionRunsMillis = HOUSEKEEPING_PERIOD_MS;
     private volatile int numTestsPerEvictionRun = 3;
-    private volatile long evictorShutdownTimeoutMillis = 10000L;
+    private volatile long evictorShutdownTimeoutMillis = DEFAULT_SHUTDOWN_TIMEOUT;
     private volatile long idleTimeout = DEFAULT_IDLE_TIMEOUT;
     private volatile long heartbeatPeriodMillis = DEFAULT_HEARTBEAT_PERIOD;
 

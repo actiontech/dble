bddb3425d80c9abd92a9af6a9d0ac87bd24a1ce5
sunzhengfang <sunzhengfang@actionsky.com>
2020-09-21 14:15:32 +0800

    add more detail for response service toString

--- src/main/java/com/actiontech/dble/services/mysqlsharding/MySQLResponseService.java
+++ src/main/java/com/actiontech/dble/services/mysqlsharding/MySQLResponseService.java
@@ -822,7 +822,8 @@ public class MySQLResponseService extends MySQLBasedService {
 
     public String toString() {
         return "MySQLResponseService[isExecuting = " + isExecuting + " attachment = " + attachment + " autocommitSynced = " + autocommitSynced + " isolationSynced = " + isolationSynced +
-                " xaStatus = " + xaStatus + " isDDL = " + isDDL + " complexQuery = " + complexQuery + "] with connection " + connection.toString();
+                " xaStatus = " + xaStatus + " isDDL = " + isDDL + " complexQuery = " + complexQuery + "] \nwith response handler [" + responseHandler + "] \nwith rrs = [" +
+                attachment + "] \nwith connection " + connection.toString();
     }
 
     private static class StatusSync {

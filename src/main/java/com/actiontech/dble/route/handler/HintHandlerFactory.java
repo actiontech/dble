c5173acdc9526675ce28479a24f40e56be1fb0e9
guoaomen <guoaomen@actionsky.com>
2020-09-23 10:07:42 +0800

    fix: remove init method in static block

--- src/main/java/com/actiontech/dble/route/handler/HintHandlerFactory.java
+++ src/main/java/com/actiontech/dble/route/handler/HintHandlerFactory.java
@@ -10,47 +10,29 @@ import java.util.Map;
 
 public final class HintHandlerFactory {
 
-    private static volatile boolean isInit = false;
-
     private static Map<String, HintHandler> dbleHintHandlerMap = new HashMap<>();
     private static Map<String, HintHandler> rwSplitHintHandlerMap = new HashMap<>();
 
-    private HintHandlerFactory() {
-    }
-
-    private static void init() {
+    static {
         dbleHintHandlerMap.put("sql", new HintSQLHandler());
         dbleHintHandlerMap.put("shardingnode", new HintShardingNodeHandler());
-        // force master or force slave
         dbleHintHandlerMap.put("db_type", new HintMasterDBHandler());
 
         rwSplitHintHandlerMap.put("db_instance_url", new HintDbInstanceHandler());
         rwSplitHintHandlerMap.put("uproxy_dest", new HintDbInstanceHandler());
         rwSplitHintHandlerMap.put("master", new HintMasterDBHandler());
         rwSplitHintHandlerMap.put("db_type", new HintMasterDBHandler());
-        isInit = true;
+    }
+
+    private HintHandlerFactory() {
     }
 
     public static HintHandler getDbleHintHandler(String hintType) {
-        if (!isInit) {
-            synchronized (HintHandlerFactory.class) {
-                if (!isInit) {
-                    init();
-                }
-            }
-        }
         return dbleHintHandlerMap.get(hintType);
     }
 
 
     public static HintHandler getRwSplitHintHandler(String hintType) {
-        if (!isInit) {
-            synchronized (HintHandlerFactory.class) {
-                if (!isInit) {
-                    init();
-                }
-            }
-        }
         return rwSplitHintHandlerMap.get(hintType);
     }
 

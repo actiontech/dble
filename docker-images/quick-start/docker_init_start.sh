e674b46f78ff1d1a7e05b6ca1b5539d824559a2b
wd2365151147 <2365151147@qq.com>
2020-10-12 15:38:49 +0800

    quick start dble & mgr

--- docker-images/quick-start/docker_init_start.sh
+++ docker-images/quick-start/docker_init_start.sh
@@ -2,7 +2,11 @@
 
 echo "dble init&start in docker"
 
-sh /opt/dble/bin/start_mgr.sh
+if [[ $1 == "startMgr" ]]; then
+    echo "start mgr ..."
+    sh /opt/dble/bin/start_mgr.sh
+fi
+
 sh /opt/dble/bin/dble start
 sh /opt/dble/bin/wait-for-it.sh 127.0.0.1:8066
 mysql -P9066 -u man1 -h 127.0.0.1 -p654321 -e "create database @@shardingNode ='dn1,dn2,dn3,dn4,dn5,dn6'"

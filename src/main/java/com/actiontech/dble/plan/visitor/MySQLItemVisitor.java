2f9ef81236126feb2c50c6e978a9c967133bbb6b
PanternBao <838800176@qq.com>
2020-10-21 16:37:16 +0800

    charset for atk-1400

--- src/main/java/com/actiontech/dble/plan/visitor/MySQLItemVisitor.java
+++ src/main/java/com/actiontech/dble/plan/visitor/MySQLItemVisitor.java
@@ -369,7 +369,7 @@ public class MySQLItemVisitor extends MySqlASTVisitorAdapter {
 
     @Override
     public void endVisit(SQLAllColumnExpr x) {
-        item = new ItemField(null, null, "*");
+        item = new ItemField(null, null, "*", charsetIndex);
         initName(x);
     }
 
@@ -441,7 +441,7 @@ public class MySQLItemVisitor extends MySqlASTVisitorAdapter {
 
     @Override
     public void endVisit(SQLIdentifierExpr x) {
-        item = new ItemField(null, null, StringUtil.removeBackQuote(x.getSimpleName()));
+        item = new ItemField(null, null, StringUtil.removeBackQuote(x.getSimpleName()), charsetIndex);
     }
 
     @Override
@@ -501,7 +501,7 @@ public class MySQLItemVisitor extends MySqlASTVisitorAdapter {
                 }
             }
         }
-        item = new ItemVariables(x.getName(), new ItemField(null, null, variable));
+        item = new ItemVariables(x.getName(), new ItemField(null, null, variable, charsetIndex));
         initName(x);
     }
 
@@ -516,7 +516,7 @@ public class MySQLItemVisitor extends MySqlASTVisitorAdapter {
         } else {
             tableName = ((SQLIdentifierExpr) x.getOwner()).getSimpleName();
         }
-        item = new ItemField(dbName, StringUtil.removeBackQuote(tableName), StringUtil.removeBackQuote(x.getSimpleName()));
+        item = new ItemField(dbName, StringUtil.removeBackQuote(tableName), StringUtil.removeBackQuote(x.getSimpleName()), charsetIndex);
     }
 
     @Override

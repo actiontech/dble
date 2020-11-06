6ca04680bfac1ea96f3ca40c39f62dcaa2318019
guoaomen <guoaomen@actionsky.com>
2020-11-06 10:33:01 +0800

    fix: inner 637 The connection is closed and the column value is null

--- src/main/java/com/actiontech/dble/services/manager/response/ShowBackend.java
+++ src/main/java/com/actiontech/dble/services/manager/response/ShowBackend.java
@@ -213,14 +213,14 @@ public final class ShowBackend {
         row.add(c.getCharsetName().getClient().getBytes());
         row.add(c.getCharsetName().getCollation().getBytes());
         row.add(c.getCharsetName().getResults().getBytes());
-        row.add(c.isClosed() ? new byte[]{0} : (c.getBackendService().getTxIsolation() + "").getBytes());
-        row.add(c.isClosed() ? new byte[]{0} : (c.getBackendService().isAutocommit() + "").getBytes());
-        row.add(c.isClosed() ? new byte[]{0} : StringUtil.encode(c.getBackendService().getStringOfSysVariables(), charset));
-        row.add(c.isClosed() ? new byte[]{0} : StringUtil.encode(c.getBackendService().getStringOfUsrVariables(), charset));
-        row.add(c.isClosed() ? new byte[]{0} : StringUtil.encode(c.getBackendService().getXaStatus().toString(), charset));
+        row.add(c.isClosed() ? null : (c.getBackendService().getTxIsolation() + "").getBytes());
+        row.add(c.isClosed() ? null : (c.getBackendService().isAutocommit() + "").getBytes());
+        row.add(c.isClosed() ? null : StringUtil.encode(c.getBackendService().getStringOfSysVariables(), charset));
+        row.add(c.isClosed() ? null : StringUtil.encode(c.getBackendService().getStringOfUsrVariables(), charset));
+        row.add(c.isClosed() ? null : StringUtil.encode(c.getBackendService().getXaStatus().toString(), charset));
         row.add(StringUtil.encode(FormatUtil.formatDate(c.getPoolDestroyedTime()), charset));
         if (c.isClosed()) {
-            row.add(new byte[]{0});
+            row.add(null);
         } else if (state == PooledConnection.INITIAL) {
             ResponseHandler handler = c.getBackendService().getResponseHandler();
             row.add(handler instanceof HeartbeatSQLJob ? "true".getBytes() : "false".getBytes());

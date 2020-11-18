239970cebb9c4af167a1e16bea52e60426e906e0
guoaomen <guoaomen@actionsky.com>
2020-11-18 11:01:13 +0800

    fix: service is set to null and connection close is not atomic service is set to null earlier than connection close

--- src/main/java/com/actiontech/dble/services/manager/response/ShowBackend.java
+++ src/main/java/com/actiontech/dble/services/manager/response/ShowBackend.java
@@ -213,13 +213,13 @@ public final class ShowBackend {
         row.add(c.getCharsetName().getClient().getBytes());
         row.add(c.getCharsetName().getCollation().getBytes());
         row.add(c.getCharsetName().getResults().getBytes());
-        row.add(c.isClosed() ? null : (c.getBackendService().getTxIsolation() + "").getBytes());
-        row.add(c.isClosed() ? null : (c.getBackendService().isAutocommit() + "").getBytes());
-        row.add(c.isClosed() ? null : StringUtil.encode(c.getBackendService().getStringOfSysVariables(), charset));
-        row.add(c.isClosed() ? null : StringUtil.encode(c.getBackendService().getStringOfUsrVariables(), charset));
-        row.add(c.isClosed() ? null : StringUtil.encode(c.getBackendService().getXaStatus().toString(), charset));
+        row.add(null == c.getBackendService() ? null : (c.getBackendService().getTxIsolation() + "").getBytes());
+        row.add(null == c.getBackendService() ? null : (c.getBackendService().isAutocommit() + "").getBytes());
+        row.add(null == c.getBackendService() ? null : StringUtil.encode(c.getBackendService().getStringOfSysVariables(), charset));
+        row.add(null == c.getBackendService() ? null : StringUtil.encode(c.getBackendService().getStringOfUsrVariables(), charset));
+        row.add(null == c.getBackendService() ? null : StringUtil.encode(c.getBackendService().getXaStatus().toString(), charset));
         row.add(StringUtil.encode(FormatUtil.formatDate(c.getPoolDestroyedTime()), charset));
-        if (c.isClosed()) {
+        if (null == c.getBackendService()) {
             row.add(null);
         } else if (state == PooledConnection.INITIAL) {
             ResponseHandler handler = c.getBackendService().getResponseHandler();

8187f8c3779282858f7e1c1977decd46c5e8fb76
tiger.yan <yanhuqing666@gmail.com>
2020-11-27 15:17:18 +0800

    inner 721 no sharding/split user (#2332)

--- src/main/java/com/actiontech/dble/services/manager/response/DryRun.java
+++ src/main/java/com/actiontech/dble/services/manager/response/DryRun.java
@@ -250,7 +250,11 @@ public final class DryRun {
                 }
             }
             if (!hasShardingUser) {
-                list.add(new ErrorInfo("Xml", "WARNING", "There is No Sharding User"));
+                if (serverConfig.getSchemas().size() > 0) {
+                    list.add(new ErrorInfo("Xml", "WARNING", "There is No Sharding User"));
+                } else {
+                    list.add(new ErrorInfo("Xml", "NOTICE", "There is No Sharding User"));
+                }
             } else if (schema.size() <= serverConfig.getSchemas().size()) {
                 for (String schemaName : serverConfig.getSchemas().keySet()) {
                     if (!schema.contains(schemaName)) {
@@ -260,7 +264,11 @@ public final class DryRun {
             }
 
             if (!hasRWSplitUser) {
-                list.add(new ErrorInfo("Xml", "WARNING", "There is No RWSplit User"));
+                if (serverConfig.getSchemas().size() == 0) {
+                    list.add(new ErrorInfo("Xml", "WARNING", "There is No RWSplit User"));
+                } else {
+                    list.add(new ErrorInfo("Xml", "NOTICE", "There is No RWSplit User"));
+                }
             }
 
             if (!hasManagerUser) {

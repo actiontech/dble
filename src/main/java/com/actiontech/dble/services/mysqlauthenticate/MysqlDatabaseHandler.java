0064fb70634ac97865bbf10906a92e9fa37c4afb
lin <yuanlinzhu1995@gmail.com>
2020-12-28 16:37:29 +0800

    fix : the database does not exist in mysql but can be logged in rwsplituser  inner 668  and 667

---
+++ src/main/java/com/actiontech/dble/services/mysqlauthenticate/MysqlDatabaseHandler.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2016-2020 ActionTech.
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher.
+ */
+
+package com.actiontech.dble.services.mysqlauthenticate;
+
+import com.actiontech.dble.backend.datasource.PhysicalDbGroup;
+import com.actiontech.dble.backend.datasource.PhysicalDbInstance;
+import com.actiontech.dble.sqlengine.MultiRowSQLQueryResultHandler;
+import com.actiontech.dble.sqlengine.OneTimeConnJob;
+import com.actiontech.dble.sqlengine.SQLQueryResult;
+import com.actiontech.dble.sqlengine.SQLQueryResultListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MysqlDatabaseHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MysqlDatabaseHandler.class);
+    private static final String MYSQL_SHOW_DATABASES = "show databases";
+    private final ReentrantLock lock = new ReentrantLock();
+    private Map<String, PhysicalDbGroup> dbGroups;
+    private Set<String> databases = new HashSet<>();
+    private final Condition finishCond = lock.newCondition();
+    private boolean isFinish = false;
+
+    public MysqlDatabaseHandler(Map<String, PhysicalDbGroup> dbGroups) {
+        this.dbGroups = dbGroups;
+    }
+
+    public Set<String> execute() {
+        String mysqlShowDataBasesCols = "Database";
+        MultiRowSQLQueryResultHandler resultHandler = new MultiRowSQLQueryResultHandler(new String[]{mysqlShowDataBasesCols}, new MySQLShowDatabasesListener(mysqlShowDataBasesCols));
+        PhysicalDbInstance ds = getPhysicalDbInstance();
+        OneTimeConnJob sqlJob = new OneTimeConnJob(MYSQL_SHOW_DATABASES, null, resultHandler, ds);
+        if (ds != null) {
+            sqlJob.run();
+            waitDone();
+        } else {
+            LOGGER.warn("No dbInstance is alive, server can not get 'show databases' result");
+        }
+        return databases;
+    }
+
+    private PhysicalDbInstance getPhysicalDbInstance() {
+        PhysicalDbInstance ds = null;
+        for (PhysicalDbGroup dbGroup : dbGroups.values()) {
+            PhysicalDbInstance dsTest = dbGroup.getWriteDbInstance();
+            if (dsTest.isTestConnSuccess()) {
+                ds = dsTest;
+            }
+            if (ds != null) {
+                break;
+            }
+        }
+        if (ds == null) {
+            for (PhysicalDbGroup dbGroup : dbGroups.values()) {
+                for (PhysicalDbInstance dsTest : dbGroup.getDbInstances(false)) {
+                    if (dsTest.isTestConnSuccess()) {
+                        ds = dsTest;
+                        break;
+                    }
+                }
+                if (ds != null) {
+                    break;
+                }
+            }
+        }
+        return ds;
+    }
+
+    private void waitDone() {
+        lock.lock();
+        try {
+            while (!isFinish) {
+                finishCond.await();
+            }
+        } catch (InterruptedException e) {
+            LOGGER.info("[MysqlDatabaseHandler] conn Interrupted: " + e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private class MySQLShowDatabasesListener implements SQLQueryResultListener<SQLQueryResult<List<Map<String, String>>>> {
+        private String mysqlShowDataBasesCol;
+
+        MySQLShowDatabasesListener(String mysqlShowDataBasesCol) {
+            this.mysqlShowDataBasesCol = mysqlShowDataBasesCol;
+        }
+
+        @Override
+        public void onResult(SQLQueryResult<List<Map<String, String>>> result) {
+            if (result.isSuccess()) {
+                List<Map<String, String>> rows = result.getResult();
+                for (Map<String, String> row : rows) {
+                    String databaseName = row.get(mysqlShowDataBasesCol);
+                    databases.add(databaseName.toLowerCase());
+                }
+            }
+            handleFinished();
+        }
+
+        private void handleFinished() {
+            lock.lock();
+            try {
+                isFinish = true;
+                finishCond.signal();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+}
+
+
+
--- src/main/java/com/actiontech/dble/services/mysqlauthenticate/util/AuthUtil.java
+++ src/main/java/com/actiontech/dble/services/mysqlauthenticate/util/AuthUtil.java
@@ -8,12 +8,10 @@ package com.actiontech.dble.services.mysqlauthenticate.util;
 import com.actiontech.dble.DbleServer;
 import com.actiontech.dble.config.Capabilities;
 import com.actiontech.dble.config.ErrorCode;
-import com.actiontech.dble.config.model.user.ManagerUserConfig;
-import com.actiontech.dble.config.model.user.ShardingUserConfig;
-import com.actiontech.dble.config.model.user.UserConfig;
-import com.actiontech.dble.config.model.user.UserName;
+import com.actiontech.dble.config.model.user.*;
 import com.actiontech.dble.net.connection.AbstractConnection;
 import com.actiontech.dble.net.connection.FrontendConnection;
+import com.actiontech.dble.services.mysqlauthenticate.MysqlDatabaseHandler;
 import com.actiontech.dble.services.manager.information.ManagerSchemaInfo;
 import com.actiontech.dble.services.mysqlauthenticate.PluginName;
 import com.actiontech.dble.services.mysqlauthenticate.SecurityUtil;
@@ -81,6 +79,20 @@ public final class AuthUtil {
                         return "Unknown database '" + schema + "'";
                     default:
                         break;
+                }
+            } else if (userConfig instanceof RwSplitUserConfig) {
+                // check RwSplitUserConfig
+                switch (checkDatabase(schema)) {
+                    case ErrorCode.ER_BAD_DB_ERROR:
+                        return "Unknown database '" + schema + "'";
+                    case ErrorCode.ER_DBACCESS_DENIED_ERROR:
+                        return "Access denied for user '" + user + "' to database '" + schema + "'";
+                    default:
+                        break;
+                }
+
+                if ((Capabilities.CLIENT_FOUND_ROWS == (Capabilities.CLIENT_FOUND_ROWS & clientFlags)) != CapClientFoundRows.getInstance().isEnableCapClientFoundRows()) {
+                    return "The client requested CLIENT_FOUND_ROWS capabilities does not match, in the manager use show @@cap_client_found_rows check latest status.";
                 }
             }
 
@@ -173,6 +185,17 @@ public final class AuthUtil {
         } else {
             return ErrorCode.ER_DBACCESS_DENIED_ERROR;
         }
+    }
+
+    private static int checkDatabase(String database) {
+        if (database == null) {
+            return 0;
+        }
+        Set<String> databases = new MysqlDatabaseHandler(DbleServer.getInstance().getConfig().getDbGroups()).execute();
+        if (!databases.contains(database)) {
+            return ErrorCode.ER_BAD_DB_ERROR;
+        }
+        return 0;
     }
 
     private static int checkManagerSchema(String schema) {

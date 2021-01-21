937c9154e599205f4ef0f3648db63ad2a1d76a7c
baofengqi <baofengqi@actionsky.com>
2021-01-21 17:17:33 +0800

    revert version

--- src/main/java/com/actiontech/dble/config/Versions.java
+++ src/main/java/com/actiontech/dble/config/Versions.java
@@ -1,12 +1,13 @@
 /*
-* Copyright (C) 2016-2020 ActionTech.
-* based on code by MyCATCopyrightHolder Copyright (c) 2013, OpenCloudDB/MyCAT.
-* License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher.
-*/
+ * Copyright (C) 2016-2021 ActionTech.
+ * based on code by MyCATCopyrightHolder Copyright (c) 2013, OpenCloudDB/MyCAT.
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher.
+ */
 package com.actiontech.dble.config;
 
-import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author
  */
@@ -14,12 +15,12 @@ public abstract class Versions {
 
     public static final byte PROTOCOL_VERSION = 10;
 
-    private static byte[] serverVersion = "5.6.29-dble-9.9.9.9-0a90ae5c273ca4e1642efd1f5521e7c166183e58-20210121163310".getBytes();
+    private static byte[] serverVersion = "5.6.29-dble-9.9.9.9-xxxxxx-yyyyMMddHHmmss".getBytes();
     public static final byte[] VERSION_COMMENT = "dble Server (ActionTech)".getBytes();
     public static final String ANNOTATION_NAME = "dble:";
     public static final String ROOT_PREFIX = "dble";
     public static final String DOMAIN = "http://dble.cloud/";
-    public static final String CONFIG_VERSION = "4.0";
+    public static final String CONFIG_VERSION = "3.0"; // will be replaced
     private static final Pattern VERSION_PATTERN = Pattern.compile("^(\\d{1,})\\.(\\d{1,})$", Pattern.CASE_INSENSITIVE);
 
     public static void setServerVersion(String version) {

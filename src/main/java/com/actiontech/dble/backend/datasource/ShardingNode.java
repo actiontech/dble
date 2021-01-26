31823ad7e1e16ff8b8a7c141e6ac3d6f22401484
lin <yuanlinzhu1995@gmail.com>
2021-01-26 12:25:37 +0800

    remove unused import

--- src/main/java/com/actiontech/dble/backend/datasource/ShardingNode.java
+++ src/main/java/com/actiontech/dble/backend/datasource/ShardingNode.java
@@ -14,7 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.Set;
 
 public class ShardingNode {
     protected static final Logger LOGGER = LoggerFactory.getLogger(ShardingNode.class);

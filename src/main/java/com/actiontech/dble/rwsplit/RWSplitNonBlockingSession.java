59fbc6571e022712e4a08b8ae9983de70473637f
lin <yuanlinzhu1995@gmail.com>
2021-02-05 10:56:03 +0800

    inprepare parameter  implementation uses the same connection

--- src/main/java/com/actiontech/dble/rwsplit/RWSplitNonBlockingSession.java
+++ src/main/java/com/actiontech/dble/rwsplit/RWSplitNonBlockingSession.java
@@ -54,13 +54,13 @@ public class RWSplitNonBlockingSession extends Session {
             if (statement instanceof MySqlPrepareStatement) {
                 String simpleName = ((MySqlPrepareStatement) statement).getName().getSimpleName();
                 nameSet.add(simpleName);
-                rwSplitService.setTxStart(true);
+                rwSplitService.setInPrepare(true);
             }
             if (statement instanceof MysqlDeallocatePrepareStatement) {
                 String simpleName = ((MysqlDeallocatePrepareStatement) statement).getStatementName().getSimpleName();
                 nameSet.remove(simpleName);
                 if (nameSet.isEmpty()) {
-                    rwSplitService.setTxStart(false);
+                    rwSplitService.setInPrepare(false);
                 }
             }
         } catch (SQLSyntaxErrorException throwables) {

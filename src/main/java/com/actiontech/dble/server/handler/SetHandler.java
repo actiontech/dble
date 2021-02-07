b0269f8c6ff6a032fb8799bcf3d01c15a3238465
Collapsar <baofengqi@actionsky.com>
2021-02-07 09:26:04 +0800

    revert inner-889 (#2473)

--- src/main/java/com/actiontech/dble/server/handler/SetHandler.java
+++ src/main/java/com/actiontech/dble/server/handler/SetHandler.java
@@ -153,7 +153,7 @@ public final class SetHandler {
             if (owner.isGlobal()) {
                 throw new SQLSyntaxErrorException("unsupport global");
             }
-            return target.toString();
+            return target.getName();
         } else if (key instanceof SQLVariantRefExpr) {
             SQLVariantRefExpr target = (SQLVariantRefExpr) key;
             if (target.isGlobal()) {

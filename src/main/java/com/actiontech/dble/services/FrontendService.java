e64524339f1f574cc834baac12ec0a130e6544de
wd2365151147 <2365151147@qq.com>
2021-02-22 10:56:59 +0800

    additional notes

--- src/main/java/com/actiontech/dble/services/FrontendService.java
+++ src/main/java/com/actiontech/dble/services/FrontendService.java
@@ -79,7 +79,7 @@ public abstract class FrontendService<T extends UserConfig> extends AbstractServ
 
     @Override
     public void execute(ServiceTask task) {
-        // prevents QUIT from losing cumulative
+        // prevents QUIT、CLOSE_STMT from losing cumulative
         if (task.getOrgData().length > 4 && (task.getOrgData()[4] == MySQLPacket.COM_QUIT || task.getOrgData()[4] == MySQLPacket.COM_STMT_CLOSE)) {
             this.handleInnerData(task.getOrgData());
             return;

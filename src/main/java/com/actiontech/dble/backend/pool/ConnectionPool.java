7cfaea9754bfb03ca2841d144c3bb21b1f96f03b
baofengqi <baofengqi@actionsky.com>
2021-02-23 13:42:11 +0800

    props of testOn* take no effect

--- src/main/java/com/actiontech/dble/backend/pool/ConnectionPool.java
+++ src/main/java/com/actiontech/dble/backend/pool/ConnectionPool.java
@@ -220,6 +220,10 @@ public class ConnectionPool extends PoolBase implements PooledConnectionListener
     @Override
     public void onHeartbeatSuccess(PooledConnection conn) {
         conn.lazySet(STATE_NOT_IN_USE);
+        // spin until a thread takes it or none are waiting
+        while (waiters.get() > 0 && conn.getState() == STATE_NOT_IN_USE && !handoffQueue.offer(conn)) {
+            Thread.yield();
+        }
     }
 
     public int getCount(final int... states) {

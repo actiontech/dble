263af9d8b9d68ac8c9369deb4e77881ec4493262
LUA <guoaomen@actionsky.com>
2021-02-23 11:26:31 +0800

    fix: auth plugin is empty when Navicat 11 is connected (#2483)

--- src/main/java/com/actiontech/dble/services/mysqlauthenticate/MySQLFrontAuthService.java
+++ src/main/java/com/actiontech/dble/services/mysqlauthenticate/MySQLFrontAuthService.java
@@ -147,15 +147,17 @@ public class MySQLFrontAuthService extends AuthService {
         auth.read(data);
         this.authPacket = auth;
         try {
-            PluginName name = PluginName.valueOf(auth.getAuthPlugin());
-            if (pluginName != name) {
-                if (LOGGER.isDebugEnabled()) {
-                    LOGGER.debug("auth switch request client-plugin:[{}],server-plugin:[{}]->[{}]", name, pluginName, PluginName.mysql_native_password);
+            if (null != auth.getAuthPlugin()) {
+                PluginName name = PluginName.valueOf(auth.getAuthPlugin());
+                if (pluginName != name) {
+                    if (LOGGER.isDebugEnabled()) {
+                        LOGGER.debug("auth switch request client-plugin:[{}],server-plugin:[{}]->[{}]", name, pluginName, PluginName.mysql_native_password);
+                    }
+                    needAuthSwitched = true;
+                    this.pluginName = PluginName.mysql_native_password;
+                    sendSwitchPacket(pluginName);
+                    return;
                 }
-                needAuthSwitched = true;
-                this.pluginName = PluginName.mysql_native_password;
-                sendSwitchPacket(pluginName);
-                return;
             }
             // check user and password whether is correct
             auth();

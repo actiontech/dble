cc2c25fe77a8320728cada55c392296b237fafc2
wd2365151147 <2365151147@qq.com>
2021-03-24 11:04:17 +0800

    adjust log level

--- src/main/java/com/actiontech/dble/backend/pool/ConnectionPool.java
+++ src/main/java/com/actiontech/dble/backend/pool/ConnectionPool.java
@@ -76,7 +76,7 @@ public class ConnectionPool extends PoolBase implements PooledConnectionListener
             for (PooledConnection conn : allConnections) {
                 if (conn.compareAndSet(STATE_NOT_IN_USE, STATE_IN_USE)) {
                     newPooledEntry(schema, waiters.get());
-                    LOGGER.info(conn.toString());
+                    LOGGER.debug(conn.toString());
                     return conn;
                 }
             }
@@ -100,7 +100,7 @@ public class ConnectionPool extends PoolBase implements PooledConnectionListener
                     if (waiting > 1) {
                         newPooledEntry(schema, waiting - 1);
                     }
-                    LOGGER.info(conn.toString());
+                    LOGGER.debug(conn.toString());
                     return conn;
                 }
             }
@@ -113,7 +113,7 @@ public class ConnectionPool extends PoolBase implements PooledConnectionListener
                 final long start = System.nanoTime();
                 final PooledConnection bagEntry = handoffQueue.poll(timeout, NANOSECONDS);
                 if (bagEntry == null || bagEntry.compareAndSet(STATE_NOT_IN_USE, STATE_IN_USE)) {
-                    LOGGER.info(null == bagEntry ? null : bagEntry.toString());
+                    LOGGER.debug(null == bagEntry ? null : bagEntry.toString());
                     return bagEntry;
                 }
 

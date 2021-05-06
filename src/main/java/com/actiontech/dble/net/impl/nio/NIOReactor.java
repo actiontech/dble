4c90db1962c8e4866570d3fa032c41e744515ef6
Rico <dcy10000@gmail.com>
2021-05-06 11:02:17 +0800

    inner-690: fix AsynchronousCloseException while read (#2619)
    Signed-off-by: dcy <dcy10000@gmail.com>

--- src/main/java/com/actiontech/dble/net/impl/nio/NIOReactor.java
+++ src/main/java/com/actiontech/dble/net/impl/nio/NIOReactor.java
@@ -16,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.channels.AsynchronousCloseException;
 import java.nio.channels.CancelledKeyException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
@@ -122,6 +123,12 @@ public final class NIOReactor {
                         if (key.isValid() && key.isReadable()) {
                             try {
                                 con.asyncRead();
+                            } catch (AsynchronousCloseException e) {
+                                //happens when close and read running in parallel.
+                                //sometimes ,no byte could be read,but an  read event triggered with  zero bytes although cause this.
+                                LOGGER.info("read bytes but the  connection is closed .connection is {}. May be the connection closed suddenly.", con);
+                                key.cancel();
+                                continue;
                             } catch (Exception e) {
                                 LOGGER.warn("caught err:", e);
                                 con.close("program err:" + e.toString());

757be52d12a618da37e8fb3da486547a13ac8266
wenyh <44251917+wenyh1@users.noreply.github.com>
2021-05-14 15:26:33 +0800

    Support XA transaction residue detection (#2630)
    Co-authored-by: Huqing Yan <yanhuqing666@gmail.com>

--- src/main/java/com/actiontech/dble/DbleServer.java
+++ src/main/java/com/actiontech/dble/DbleServer.java
@@ -7,18 +7,12 @@ package com.actiontech.dble;
 
 import com.actiontech.dble.alarm.AlertUtil;
 import com.actiontech.dble.backend.datasource.PhysicalDbGroup;
-import com.actiontech.dble.backend.datasource.ShardingNode;
-import com.actiontech.dble.backend.mysql.xa.*;
-import com.actiontech.dble.backend.mysql.xa.recovery.Repository;
-import com.actiontech.dble.backend.mysql.xa.recovery.impl.FileSystemRepository;
-import com.actiontech.dble.backend.mysql.xa.recovery.impl.KVStoreRepository;
+import com.actiontech.dble.backend.mysql.xa.XaCheckHandler;
 import com.actiontech.dble.buffer.DirectByteBufferPool;
 import com.actiontech.dble.config.DbleTempConfig;
 import com.actiontech.dble.config.ServerConfig;
 import com.actiontech.dble.config.model.ClusterConfig;
 import com.actiontech.dble.config.model.SystemConfig;
-import com.actiontech.dble.config.model.sharding.SchemaConfig;
-import com.actiontech.dble.config.model.sharding.table.BaseTableConfig;
 import com.actiontech.dble.config.util.ConfigUtil;
 import com.actiontech.dble.log.general.GeneralLogProcessor;
 import com.actiontech.dble.log.transaction.TxnLogProcessor;
@@ -252,8 +246,13 @@ public final class DbleServer {
         LOGGER.info("====================================Cache service init finish=====================================");
 
         LOGGER.info("=====================================Perform XA recovery log======================================");
-        performXARecoveryLog();
+        XaCheckHandler.performXARecoveryLog();
         LOGGER.info("====================================Perform XA recovery finish====================================");
+
+        LOGGER.info("====================================Check Residual XA====================================");
+        XaCheckHandler.checkResidualXA();
+        LOGGER.info("====================================Check Residual XA finish====================================");
+
         LOGGER.info("===================================Sync cluster pause status start====================================");
         PauseShardingNodeManager.getInstance().fetchClusterStatus();
         LOGGER.info("===================================Sync cluster pause status end  ====================================");
@@ -451,116 +450,6 @@ public final class DbleServer {
         }
     }
 
-
-    // XA recovery log check
-    private void performXARecoveryLog() {
-        // fetch the recovery log
-        CoordinatorLogEntry[] coordinatorLogEntries = getCoordinatorLogEntries();
-        // init into in memory cached
-        for (CoordinatorLogEntry coordinatorLogEntry1 : coordinatorLogEntries) {
-            genXidSeq(coordinatorLogEntry1.getId());
-            XAStateLog.flushMemoryRepository(coordinatorLogEntry1.getId(), coordinatorLogEntry1);
-        }
-        for (CoordinatorLogEntry coordinatorLogEntry : coordinatorLogEntries) {
-            boolean needRollback = false;
-            boolean needCommit = false;
-            if (coordinatorLogEntry.getTxState() == TxState.TX_COMMIT_FAILED_STATE ||
-                    // will committing, may send but failed receiving, should commit agagin
-                    coordinatorLogEntry.getTxState() == TxState.TX_COMMITTING_STATE) {
-                needCommit = true;
-            } else if (coordinatorLogEntry.getTxState() == TxState.TX_ROLLBACK_FAILED_STATE ||
-                    //don't konw prepare is succeed or not ,should rollback
-                    coordinatorLogEntry.getTxState() == TxState.TX_PREPARE_UNCONNECT_STATE ||
-                    // will rollbacking, may send but failed receiving,should rollback again
-                    coordinatorLogEntry.getTxState() == TxState.TX_ROLLBACKING_STATE ||
-                    // will preparing, may send but failed receiving,should rollback again
-                    coordinatorLogEntry.getTxState() == TxState.TX_PREPARING_STATE) {
-                needRollback = true;
-
-            }
-            if (needCommit || needRollback) {
-                tryRecovery(coordinatorLogEntry, needCommit);
-            }
-        }
-    }
-
-    private void tryRecovery(CoordinatorLogEntry coordinatorLogEntry, boolean needCommit) {
-        StringBuilder xaCmd = new StringBuilder();
-        if (needCommit) {
-            xaCmd.append("XA COMMIT ");
-        } else {
-            xaCmd.append("XA ROLLBACK ");
-        }
-        for (int j = 0; j < coordinatorLogEntry.getParticipants().length; j++) {
-            ParticipantLogEntry participantLogEntry = coordinatorLogEntry.getParticipants()[j];
-            // XA commit
-            if (participantLogEntry.getTxState() != TxState.TX_COMMIT_FAILED_STATE &&
-                    participantLogEntry.getTxState() != TxState.TX_COMMITTING_STATE &&
-                    participantLogEntry.getTxState() != TxState.TX_PREPARE_UNCONNECT_STATE &&
-                    participantLogEntry.getTxState() != TxState.TX_ROLLBACKING_STATE &&
-                    participantLogEntry.getTxState() != TxState.TX_ROLLBACK_FAILED_STATE &&
-                    participantLogEntry.getTxState() != TxState.TX_ENDED_STATE &&
-                    participantLogEntry.getTxState() != TxState.TX_PREPARED_STATE &&
-                    participantLogEntry.getTxState() != TxState.TX_PREPARING_STATE) {
-                continue;
-            }
-            outLoop:
-            for (SchemaConfig schema : DbleServer.getInstance().getConfig().getSchemas().values()) {
-                for (BaseTableConfig table : schema.getTables().values()) {
-                    for (String shardingNode : table.getShardingNodes()) {
-                        ShardingNode dn = DbleServer.getInstance().getConfig().getShardingNodes().get(shardingNode);
-                        if (participantLogEntry.compareAddress(dn.getDbGroup().getWriteDbInstance().getConfig().getIp(), dn.getDbGroup().getWriteDbInstance().getConfig().getPort(), dn.getDatabase())) {
-                            xaCmd.append(coordinatorLogEntry.getId(), 0, coordinatorLogEntry.getId().length() - 1);
-                            xaCmd.append(".");
-                            xaCmd.append(dn.getDatabase());
-                            if (participantLogEntry.getExpires() != 0) {
-                                xaCmd.append(".");
-                                xaCmd.append(participantLogEntry.getExpires());
-                            }
-                            xaCmd.append("'");
-                            XARecoverHandler handler = new XARecoverHandler(needCommit, participantLogEntry);
-                            handler.execute(xaCmd.toString(), dn.getDatabase(), dn.getDbGroup().getWriteDbInstance());
-                            if (!handler.isSuccess()) {
-                                throw new RuntimeException("Fail to recover xa when dble start, please check backend mysql.");
-                            }
-
-                            if (LOGGER.isDebugEnabled()) {
-                                LOGGER.debug(String.format("[%s] Host:[%s] sharding:[%s]", xaCmd, dn.getName(), dn.getDatabase()));
-                            }
-
-                            //reset xaCmd
-                            xaCmd.setLength(0);
-                            if (needCommit) {
-                                xaCmd.append("XA COMMIT ");
-                            } else {
-                                xaCmd.append("XA ROLLBACK ");
-                            }
-                            break outLoop;
-                        }
-                    }
-                }
-            }
-        }
-        XAStateLog.saveXARecoveryLog(coordinatorLogEntry.getId(), needCommit ? TxState.TX_COMMITTED_STATE : TxState.TX_ROLLBACKED_STATE);
-        XAStateLog.writeCheckpoint(coordinatorLogEntry.getId());
-    }
-
-    /**
-     * covert the collection to array
-     **/
-    private CoordinatorLogEntry[] getCoordinatorLogEntries() {
-        Repository fileRepository = ClusterConfig.getInstance().isClusterEnable() && ClusterConfig.getInstance().useZkMode() ? new KVStoreRepository() : new FileSystemRepository();
-        Collection<CoordinatorLogEntry> allCoordinatorLogEntries = fileRepository.getAllCoordinatorLogEntries(true);
-        fileRepository.close();
-        if (allCoordinatorLogEntries == null) {
-            return new CoordinatorLogEntry[0];
-        }
-        if (allCoordinatorLogEntries.size() == 0) {
-            return new CoordinatorLogEntry[0];
-        }
-        return allCoordinatorLogEntries.toArray(new CoordinatorLogEntry[allCoordinatorLogEntries.size()]);
-    }
-
     public String genXaTxId() {
         long seq = this.xaIDInc.incrementAndGet();
         if (seq < 0) {
@@ -580,14 +469,13 @@ public final class DbleServer {
         return id.toString();
     }
 
-    private void genXidSeq(String xaID) {
+    public void genXidSeq(String xaID) {
         String[] idSplit = xaID.replace("'", "").split("\\.");
         long seq = Long.parseLong(idSplit[2]);
         if (xaIDInc.get() < seq) {
             xaIDInc.set(seq);
         }
     }
-
 
     /**
      * get next AsynchronousChannel ,first is exclude if multi
@@ -704,4 +592,7 @@ public final class DbleServer {
         this.concurrentBackHandlerQueue = concurrentBackHandlerQueue;
     }
 
+    public long getXaIDInc() {
+        return xaIDInc.get();
+    }
 }

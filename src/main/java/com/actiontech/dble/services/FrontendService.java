6caef1d68c260b539845ebd6ed5e9ab74bb85427
guoaomen <guoaomen@actionsky.com>
2021-05-20 10:20:45 +0800

    fix: when an error occurs, the front end returns an error packet

--- src/main/java/com/actiontech/dble/services/FrontendService.java
+++ src/main/java/com/actiontech/dble/services/FrontendService.java
@@ -6,6 +6,7 @@
 package com.actiontech.dble.services;
 
 import com.actiontech.dble.DbleServer;
+import com.actiontech.dble.config.ErrorCode;
 import com.actiontech.dble.config.model.SystemConfig;
 import com.actiontech.dble.config.model.user.UserConfig;
 import com.actiontech.dble.config.model.user.UserName;
@@ -182,8 +183,15 @@ public abstract class FrontendService<T extends UserConfig> extends AbstractServ
 
             super.consumeSingleTask(serviceTask);
         } catch (Throwable e) {
-            LOGGER.error("frontExecutor process error: ", e);
             connectionSerializableLock.unLock();
+            String msg = e.getMessage();
+            if (StringUtil.isEmpty(msg)) {
+                LOGGER.warn("Maybe occur a bug, please check it.", e);
+                msg = e.toString();
+            } else {
+                LOGGER.warn("There is an error you may need know.", e);
+            }
+            writeErrMessage(ErrorCode.ER_UNKNOWN_ERROR, msg);
         }
     }
 

9fb27c473c78b0956bfa2b1c2b0ba3312f52d237
yanhuqing666 <yanhuqing666@gmail.com>
2021-06-02 14:01:20 +0800

    test jdbc

--- src/test/java/demo/test/TestJdbc.java
+++ src/test/java/demo/test/TestJdbc.java
@@ -1,10 +1,21 @@
 package demo.test;
 
+import com.mysql.cj.jdbc.MysqlPooledConnection;
+
 import java.sql.*;
 
 public class TestJdbc {
     public static void main(String args[]) throws ClassNotFoundException, SQLException {
 
+        TestJdbc obj = new TestJdbc();
+        try {
+            obj.test2();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+    public void test0() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
         Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:8066/test?useServerPrepStmts=true&&useAffectedRows=true", "rw", "123456");
 
@@ -31,6 +42,43 @@ public class TestJdbc {
         rs.close();
         stmt.close();
         conn.close();
+    }
+    public void test1() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://10.186.62.102:3307/?connectionAttributes=key1:value1", "root", "123456");
+        PreparedStatement stmt = conn.prepareStatement("select * from test where id=?");
+        stmt.setInt(1, 1);
+
+        ResultSet rs = stmt.executeQuery();
+        while (rs.next()) {
+            System.out.println(rs.getInt(1));
+        }
+        rs = stmt.executeQuery();
+        while (rs.next()) {
+            System.out.println(rs.getInt(1));
+        }
+
+        rs.close();
+        stmt.close();
+
+
+        conn.close();
+    }
+    public void test2() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connectionPrepare = DriverManager.getConnection(
+                    "jdbc:mysql://10.186.62.102:3306/?connectionAttributes=key1:value1", "root", "123456");
+            MysqlPooledConnection pool = new MysqlPooledConnection((com.mysql.cj.jdbc.JdbcConnection) connectionPrepare);
+            Connection connection = pool.getConnection();
+            connection.setCatalog("db_1");
+            Statement stmt = connection.createStatement();
+            stmt.execute("select * from test");
+            stmt.close();
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
     }
 }

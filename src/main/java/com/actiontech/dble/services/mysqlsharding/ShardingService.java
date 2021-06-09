fa4c4f9bafe59d219f44851a70c2efb8c4598a02
wd2365151147 <2365151147@qq.com>
2021-06-09 14:01:59 +0800

    handle duplicate errorCode

--- src/main/java/com/actiontech/dble/services/mysqlsharding/ShardingService.java
+++ src/main/java/com/actiontech/dble/services/mysqlsharding/ShardingService.java
@@ -180,7 +180,7 @@ public class ShardingService extends BusinessService<ShardingUserConfig> {
                     writeErrMessage(ErrorCode.ER_PARSE_ERROR, "druid not support sql syntax, the reason is " +
                             result.getViolations().get(0).getMessage());
                 } else {
-                    String violation = "[" + WallErrorCode.get(result.getViolations().get(0).getErrorCode()) + "]";
+                    String violation = "[" + WallErrorCode.get(result.getViolations().get(0)) + "]";
                     String msg = "Intercepted by suspected configuration " + violation + " in the blacklist of user '" + user + "', so it is considered unsafe SQL";
                     LOGGER.warn("Firewall message:{}, {}",
                             result.getViolations().get(0).getMessage(), msg);

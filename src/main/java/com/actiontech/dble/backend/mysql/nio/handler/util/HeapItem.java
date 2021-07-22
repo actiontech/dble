4654d5410ef7a69b0acef81063187d4fb6caf117
Collapsar <baofengqi@actionsky.com>
2021-07-22 15:41:00 +0800

    inner-1997 : fix MultiNodeMergeAndOrderHandler ownThread leak (#2794)
    * inner-1997 : fix MultiNodeMergeAndOrderHandler ownThread leak
    
    * add correct null item
    
    * Adjust the order

--- src/main/java/com/actiontech/dble/backend/mysql/nio/handler/util/HeapItem.java
+++ src/main/java/com/actiontech/dble/backend/mysql/nio/handler/util/HeapItem.java
@@ -9,19 +9,28 @@ import com.actiontech.dble.backend.mysql.nio.MySQLConnection;
 import com.actiontech.dble.net.mysql.RowDataPacket;
 
 public class HeapItem {
+
+    private static final HeapItem NULL_ITEM;
+
     private byte[] row;
     private RowDataPacket rowPacket;
     private MySQLConnection hashIndex;
     private boolean isNull = false;
 
+    static {
+        NULL_ITEM = new HeapItem();
+        NULL_ITEM.isNull = true;
+    }
+
     public static HeapItem nullItem() {
-        HeapItem nullItem = new HeapItem(null, null, null);
-        nullItem.isNull = true;
-        return nullItem;
+        return NULL_ITEM;
     }
 
     public boolean isNullItem() {
         return row == null && isNull;
+    }
+
+    private HeapItem() {
     }
 
     public HeapItem(byte[] row, RowDataPacket rdp, MySQLConnection index) {

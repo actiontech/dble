d5a1d000971fce7c06ba38c83b09fe87d17f0094
yanhuqing666 <yanhuqing666@gmail.com>
2021-09-01 18:26:54 +0800

    buffer bug and wrong log

--- src/main/java/com/actiontech/dble/manager/response/FlowControlList.java
+++ src/main/java/com/actiontech/dble/manager/response/FlowControlList.java
@@ -70,10 +70,41 @@ public final class FlowControlList {
         byte packetId = EOF.getPacketId();
 
         if (WriteQueueFlowController.isEnableFlowControl()) {
-            //find all server connection
-            packetId = findAllServerConnection(buffer, c, packetId);
+            {
+                //find all server connection
+                NIOProcessor[] processors = DbleServer.getInstance().getFrontProcessors();
+                for (NIOProcessor p : processors) {
+                    for (FrontendConnection fc : p.getFrontends().values()) {
+                        if (fc instanceof ServerConnection && fc.isFlowControlled()) {
+                            RowDataPacket row = new RowDataPacket(FIELD_COUNT);
+                            row.add(StringUtil.encode("ServerConnection", c.getCharset().getResults()));
+                            row.add(LongUtil.toBytes(fc.getId()));
+                            row.add(StringUtil.encode(fc.getHost() + ":" + fc.getLocalPort() + "/" + fc.getSchema() + " user = " + fc.getUser(), c.getCharset().getResults()));
+                            row.add(LongUtil.toBytes(fc.getWriteQueue().size()));
+                            row.setPacketId(++packetId);
+                            buffer = row.write(buffer, c, true);
+                        }
+                    }
+                }
+            }
             //find all mysql connection
-            packetId = findAllMySQLConnection(buffer, c, packetId);
+            {
+                NIOProcessor[] processors = DbleServer.getInstance().getBackendProcessors();
+                for (NIOProcessor p : processors) {
+                    for (BackendConnection bc : p.getBackends().values()) {
+                        MySQLConnection mc = (MySQLConnection) bc;
+                        if (mc.isFlowControlled()) {
+                            RowDataPacket row = new RowDataPacket(FIELD_COUNT);
+                            row.add(StringUtil.encode("MySQLConnection", c.getCharset().getResults()));
+                            row.add(LongUtil.toBytes(mc.getThreadId()));
+                            row.add(StringUtil.encode(mc.getDbInstance().getConfig().getUrl() + "/" + mc.getSchema() + " id = " + mc.getThreadId(), c.getCharset().getResults()));
+                            row.add(LongUtil.toBytes(mc.getWriteQueue().size()));
+                            row.setPacketId(++packetId);
+                            buffer = row.write(buffer, c, true);
+                        }
+                    }
+                }
+            }
         }
 
         // write last eof
@@ -85,41 +116,4 @@ public final class FlowControlList {
         c.write(buffer);
     }
 
-
-    private static byte findAllServerConnection(ByteBuffer buffer, ManagerConnection c, byte packetId) {
-        NIOProcessor[] processors = DbleServer.getInstance().getFrontProcessors();
-        for (NIOProcessor p : processors) {
-            for (FrontendConnection fc : p.getFrontends().values()) {
-                if (fc instanceof ServerConnection && fc.isFlowControlled()) {
-                    RowDataPacket row = new RowDataPacket(FIELD_COUNT);
-                    row.add(StringUtil.encode("ServerConnection", c.getCharset().getResults()));
-                    row.add(LongUtil.toBytes(fc.getId()));
-                    row.add(StringUtil.encode(fc.getHost() + ":" + fc.getLocalPort() + "/" + fc.getSchema() + " user = " + fc.getUser(), c.getCharset().getResults()));
-                    row.add(LongUtil.toBytes(fc.getWriteQueue().size()));
-                    row.setPacketId(++packetId);
-                    buffer = row.write(buffer, c, true);
-                }
-            }
-        }
-        return packetId;
-    }
-
-    private static byte findAllMySQLConnection(ByteBuffer buffer, ManagerConnection c, byte packetId) {
-        NIOProcessor[] processors = DbleServer.getInstance().getBackendProcessors();
-        for (NIOProcessor p : processors) {
-            for (BackendConnection bc : p.getBackends().values()) {
-                MySQLConnection mc = (MySQLConnection) bc;
-                if (mc.isFlowControlled()) {
-                    RowDataPacket row = new RowDataPacket(FIELD_COUNT);
-                    row.add(StringUtil.encode("MySQLConnection", c.getCharset().getResults()));
-                    row.add(LongUtil.toBytes(mc.getThreadId()));
-                    row.add(StringUtil.encode(mc.getDbInstance().getConfig().getUrl() + "/" + mc.getSchema() + " id = " + mc.getThreadId(), c.getCharset().getResults()));
-                    row.add(LongUtil.toBytes(mc.getWriteQueue().size()));
-                    row.setPacketId(++packetId);
-                    buffer = row.write(buffer, c, true);
-                }
-            }
-        }
-        return packetId;
-    }
 }

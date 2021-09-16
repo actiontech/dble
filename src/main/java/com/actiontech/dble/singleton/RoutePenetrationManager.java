9764a4e3aa8bf299984d71181c2d5b20784525a8
dcy <dcy10000@gmail.com>
2021-09-16 14:05:06 +0800

    inner-1376-supplement:  change format to json
    Signed-off-by: dcy <dcy10000@gmail.com>

--- src/main/java/com/actiontech/dble/singleton/RoutePenetrationManager.java
+++ src/main/java/com/actiontech/dble/singleton/RoutePenetrationManager.java
@@ -8,10 +8,13 @@ package com.actiontech.dble.singleton;
 import com.actiontech.dble.DbleServer;
 import com.actiontech.dble.config.model.SystemConfig;
 import com.google.common.collect.Lists;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -20,7 +23,7 @@ import java.util.regex.Pattern;
  */
 public final class RoutePenetrationManager {
     private static final RoutePenetrationManager INSTANCE = new RoutePenetrationManager();
-    private final List<PenetrationRule> list = Lists.newArrayList();
+    private List<PenetrationRule> rules = Lists.newArrayList();
     private static final Logger LOGGER = LogManager.getLogger(RoutePenetrationManager.class);
 
     private RoutePenetrationManager() {
@@ -34,28 +37,16 @@ public final class RoutePenetrationManager {
     public void init() {
         final SystemConfig config = DbleServer.getInstance().getConfig().getSystem();
         try {
+            Gson gson = new Gson();
             if (config.isEnableRoutePenetration()) {
                 final String routePenetrationRules = config.getRoutePenetrationRules();
-                //split with ;  ,exclude the \;
-                final String[] rules = routePenetrationRules.trim().split("(?<!\\\\);");
-                if (rules.length % 2 != 0) {
-                    throw new IllegalStateException("rule must be pairwise.");
-                }
-                for (int i = 0; i + 1 < rules.length; i += 2) {
-                    String regex = rules[i];
-                    //escape \; to ;
-                    regex = regex.replaceAll("\\\\;", ";");
-                    final boolean caseSensitive = (Integer.parseInt(rules[i + 1]) & 1) != 0;
-                    int flag = 0;
-                    if (!caseSensitive) {
-                        flag |= Pattern.CASE_INSENSITIVE;
-                    }
-                    final Pattern pattern = Pattern.compile(regex, flag);
-                    list.add(new PenetrationRule(pattern));
-                }
+                rules = gson.fromJson(routePenetrationRules, new TypeToken<List<PenetrationRule>>() {
+                }.getType());
+                rules.forEach(PenetrationRule::init);
             }
+            LOGGER.info("init {}  route-penetration rules success", rules.size());
         } catch (Exception e) {
-            LOGGER.error("can't parse the sql-penetration rule, detail exception is ", e);
+            LOGGER.error("can't parse the route-penetration rule, detail exception is " + e);
             throw e;
         }
     }
@@ -65,18 +56,60 @@ public final class RoutePenetrationManager {
     }
 
     public boolean match(String sql) {
-        return list.stream().anyMatch((rule) -> rule.match(sql));
+        return rules.stream().anyMatch((rule) -> rule.match(sql));
     }
 
     private static final class PenetrationRule {
-        private final Pattern pattern;
+        private Pattern pattern;
+        private String regex;
+        private boolean caseSensitive = true;
+        private boolean partMatch = true;
 
-        private PenetrationRule(Pattern pattern) {
-            this.pattern = pattern;
+        public String getRegex() {
+            return regex;
+        }
+
+        public void setRegex(String regex) {
+            this.regex = regex;
+        }
+
+        public boolean isCaseSensitive() {
+            return caseSensitive;
+        }
+
+        public void setCaseSensitive(boolean caseSensitive) {
+            this.caseSensitive = caseSensitive;
+        }
+
+        public boolean isPartMatch() {
+            return partMatch;
+        }
+
+        public void setPartMatch(boolean partMatch) {
+            this.partMatch = partMatch;
+        }
+
+
+        public void init() {
+            int flag = 0;
+            if (!caseSensitive) {
+                flag |= Pattern.CASE_INSENSITIVE;
+            }
+            pattern = Pattern.compile(regex, flag);
         }
 
         public boolean match(String sql) {
-            return pattern.matcher(sql).matches();
+            final Matcher matcher = pattern.matcher(sql);
+            return partMatch ? matcher.find() : matcher.matches();
+        }
+
+        @Override
+        public String toString() {
+            return "PenetrationRule{" +
+                    ", regex='" + regex + '\'' +
+                    ", caseSensitive=" + caseSensitive +
+                    ", partMatch=" + partMatch +
+                    '}';
         }
     }
 }

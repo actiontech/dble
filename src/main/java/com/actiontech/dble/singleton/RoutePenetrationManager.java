176046e4ffabaff708a3921cab4cc7fd721ccbb6
dcy <dcy10000@gmail.com>
2021-09-16 14:38:31 +0800

    inner-1376-supplement:  change json struct
    Signed-off-by: dcy <dcy10000@gmail.com>

--- src/main/java/com/actiontech/dble/singleton/RoutePenetrationManager.java
+++ src/main/java/com/actiontech/dble/singleton/RoutePenetrationManager.java
@@ -9,10 +9,11 @@ import com.actiontech.dble.DbleServer;
 import com.actiontech.dble.config.model.SystemConfig;
 import com.google.common.collect.Lists;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -40,13 +41,16 @@ public final class RoutePenetrationManager {
             Gson gson = new Gson();
             if (config.isEnableRoutePenetration()) {
                 final String routePenetrationRules = config.getRoutePenetrationRules();
-                rules = gson.fromJson(routePenetrationRules, new TypeToken<List<PenetrationRule>>() {
-                }.getType());
+                final PenetrationConfig penetrationConfig = gson.fromJson(routePenetrationRules, PenetrationConfig.class);
+                if (penetrationConfig.getRules() == null) {
+                    throw new IllegalStateException("rules can't be null");
+                }
+                rules = Arrays.asList(penetrationConfig.getRules());
                 rules.forEach(PenetrationRule::init);
             }
             LOGGER.info("init {}  route-penetration rules success", rules.size());
         } catch (Exception e) {
-            LOGGER.error("can't parse the route-penetration rule, detail exception is " + e);
+            LOGGER.error("can't parse the route-penetration rule, please check the 'routePenetrationRules', detail exception is " + e);
             throw e;
         }
     }
@@ -57,6 +61,19 @@ public final class RoutePenetrationManager {
 
     public boolean match(String sql) {
         return rules.stream().anyMatch((rule) -> rule.match(sql));
+    }
+
+    private static final class PenetrationConfig {
+
+        private PenetrationRule[] rules = new PenetrationRule[0];
+
+        public PenetrationRule[] getRules() {
+            return rules;
+        }
+
+        public void setRules(PenetrationRule[] rules) {
+            this.rules = rules;
+        }
     }
 
     private static final class PenetrationRule {
@@ -91,6 +108,9 @@ public final class RoutePenetrationManager {
 
 
         public void init() {
+            if (StringUtils.isBlank(regex)) {
+                throw new IllegalStateException("regex can't be null or empty.");
+            }
             int flag = 0;
             if (!caseSensitive) {
                 flag |= Pattern.CASE_INSENSITIVE;

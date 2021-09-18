2b8714e6e0acc74322f5c51d3a9c3cda562f6d0f
dcy <dcy10000@gmail.com>
2021-09-18 13:15:44 +0800

    inner-1394: report warn if set -1 to property enableRoutePenetration
    Signed-off-by: dcy <dcy10000@gmail.com>

--- src/main/java/com/actiontech/dble/singleton/RoutePenetrationManager.java
+++ src/main/java/com/actiontech/dble/singleton/RoutePenetrationManager.java
@@ -58,7 +58,7 @@ public final class RoutePenetrationManager {
         } catch (Exception e) {
             final String msg = "can't parse the route-penetration rule, please check the 'routePenetrationRules', detail exception is ：" + e;
             LOGGER.error(msg);
-            throw new IllegalStateException(msg);
+            throw new IllegalStateException("The system property routePenetrationRules in server.xml is illegal or unset, for more detail, please check dble.log .");
         }
     }
 

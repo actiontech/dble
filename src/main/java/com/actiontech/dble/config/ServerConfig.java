5c9a73e14b36085b6ae394453f2dbf93ca77803c
baofengqi <baofengqi@actionsky.com>
2021-10-20 14:11:25 +0800

    inner 1152 - fix ci

--- src/main/java/com/actiontech/dble/config/ServerConfig.java
+++ src/main/java/com/actiontech/dble/config/ServerConfig.java
@@ -469,7 +469,7 @@ public class ServerConfig {
 
     }
 
-    private void loadSequence() {
+    public void loadSequence() {
         SequenceManager.load(DbleServer.getInstance().getSystemVariables().isLowerCaseTableNames());
     }
 

91b86f586effe222a36dbe2b4a8ca40dec1b9991
dcy <dcy10000@gmail.com>
2021-11-12 16:55:48 +0800

    inner-1462:fix small memory leak on hint with multi-node select
    Signed-off-by: dcy <dcy10000@gmail.com>

--- src/main/java/com/actiontech/dble/backend/mysql/nio/handler/MultiNodeQueryHandler.java
+++ src/main/java/com/actiontech/dble/backend/mysql/nio/handler/MultiNodeQueryHandler.java
@@ -73,8 +73,13 @@ public class MultiNodeQueryHandler extends MultiNodeHandler implements LoadDataR
             LOGGER.debug("execute multi node query " + rrs.getStatement());
         }
         this.rrs = rrs;
-        if (ServerParse.SELECT == rrs.getSqlType() && createBufferIfNeed) {
-            byteBuffer = session.getSource().allocate();
+        if (createBufferIfNeed) {
+            for (RouteResultsetNode node : rrs.getNodes()) {
+                if (ServerParse.SELECT == node.getSqlType()) {
+                    byteBuffer = session.getSource().allocate();
+                    break;
+                }
+            }
         }
         this.sessionAutocommit = session.getSource().isAutocommit();
         this.modifiedSQL = rrs.getNodes()[0].isModifySQL();

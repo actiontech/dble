73c14eeaac3b3c3c56e8a4653001b85f59f143fb
ylinzhu <46126022+ylinzhu@users.noreply.github.com>
2021-11-16 11:16:10 +0800

    fix READ_LOAD and WRITE_LOAD are always displayed as 0 inner 1445 (#2938) (#2955)

--- src/main/java/com/actiontech/dble/services/rwsplit/handle/RwSplitSelectHandler.java
+++ src/main/java/com/actiontech/dble/services/rwsplit/handle/RwSplitSelectHandler.java
@@ -26,10 +26,10 @@ public final class RwSplitSelectHandler {
                 int rs2 = RwSplitServerParseSelect.parseSpecial(stmt);
                 switch (rs2) {
                     case RwSplitServerParseSelect.LOCK_READ:
-                        service.getSession().execute(true, null);
+                        service.getSession().execute(true, null, false);
                         break;
                     default:
-                        service.getSession().execute(null, null);
+                        service.getSession().execute(null, null, false);
                         break;
                 }
                 break;

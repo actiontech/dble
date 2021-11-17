3a72236249052217c4206c399b8a6d8d4df82a5c
baofengqi <baofengqi@actionsky.com>
2021-11-17 18:06:43 +0800

    inner 1454 - release serializable lock when front connection is closed

--- src/main/java/com/actiontech/dble/services/mysqlsharding/ShardingService.java
+++ src/main/java/com/actiontech/dble/services/mysqlsharding/ShardingService.java
@@ -586,6 +586,7 @@ public class ShardingService extends BusinessService<ShardingUserConfig> {
         if (prepareHandler != null) {
             prepareHandler.clear();
         }
+        SerializableLock.getInstance().unLock(connection.getId());
     }
 
     @Override

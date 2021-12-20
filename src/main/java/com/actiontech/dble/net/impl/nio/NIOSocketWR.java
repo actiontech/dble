1611f86d81a2550a19057a6dbd36167cc05b6f16
Rico <dcy10000@gmail.com>
2021-12-20 18:06:22 +0800

    inner-1544: fix LVS/SLB heartbeat cause memory leak (#3018)
    Signed-off-by: dcy <dcy10000@gmail.com>

--- src/main/java/com/actiontech/dble/net/impl/nio/NIOSocketWR.java
+++ src/main/java/com/actiontech/dble/net/impl/nio/NIOSocketWR.java
@@ -340,9 +340,19 @@ public class NIOSocketWR extends SocketWR {
 
     @Override
     public void asyncRead() throws IOException {
-        ByteBuffer theBuffer = con.findReadBuffer();
-        int got = channel.read(theBuffer);
-        con.onReadData(got);
+        if (con.isClosed()) {
+            throw new IOException("read from closed channel cause error");
+        }
+        try {
+            ByteBuffer theBuffer = con.findReadBuffer();
+            int got = channel.read(theBuffer);
+            con.onReadData(got);
+        } finally {
+            //prevent  asyncClose and read operation happened Concurrently.
+            if (con.isClosed() && con.getReadBuffer() != null) {
+                con.recycleReadBuffer();
+            }
+        }
     }
 
     @Override

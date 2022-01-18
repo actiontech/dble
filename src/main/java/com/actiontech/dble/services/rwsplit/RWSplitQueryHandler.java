63ea554d9e6e83ccf11d19de53965ee62416feb3
yanhuqing666 <yanhuqing666@gmail.com>
2022-01-18 21:57:46 +0800

    add copyright

--- src/main/java/com/actiontech/dble/services/rwsplit/RWSplitQueryHandler.java
+++ src/main/java/com/actiontech/dble/services/rwsplit/RWSplitQueryHandler.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2016-2022 ActionTech.
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher.
+ */
+
 package com.actiontech.dble.services.rwsplit;
 
 import com.actiontech.dble.config.ErrorCode;

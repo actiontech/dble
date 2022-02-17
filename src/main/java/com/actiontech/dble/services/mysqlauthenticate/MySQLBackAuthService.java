9359c80f32be122e0e44eec318d9b19cc0e3b885
Rico <dcy10000@gmail.com>
2022-02-17 11:08:18 +0800

    inner-1554: optimize log for heartbeat of SLB/LVS (#3133)
    Signed-off-by: dcy <dcy10000@gmail.com>

--- src/main/java/com/actiontech/dble/services/mysqlauthenticate/MySQLBackAuthService.java
+++ src/main/java/com/actiontech/dble/services/mysqlauthenticate/MySQLBackAuthService.java
@@ -288,6 +288,12 @@ public class MySQLBackAuthService extends BackendService implements AuthService 
         }
     }
 
+    @Override
+    public boolean haveNotReceivedMessage() {
+        throw new UnsupportedOperationException();
+    }
+
+
 
     protected boolean isSupportFlowControl() {
         return false;

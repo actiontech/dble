32ef431128738300d27ae2ef378fd1a3d0413bfb
guoaomen <guoaomen@actionsky.com>
2022-03-31 15:36:40 +0800

    fix: add backtracking checks

--- src/main/java/com/actiontech/dble/route/util/RouterUtil.java
+++ src/main/java/com/actiontech/dble/route/util/RouterUtil.java
@@ -153,7 +153,7 @@ public final class RouterUtil {
     private static boolean checkPrefix(String content, int index) {
         if (index > 0) {
             char prefix = content.charAt(index - 1);
-            return prefix == ' ' || prefix == ',' || prefix == '(';
+            return prefix == ' ' || prefix == ',' || prefix == '(' || prefix == '=';
         }
         return false;
     }

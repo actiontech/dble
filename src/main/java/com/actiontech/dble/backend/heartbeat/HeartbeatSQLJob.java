a8f96bf3b9c81c97e39bb913a9afc3d7b1cdd07e
baofengqi <baofengqi@actionsky.com>
2022-11-04 10:50:02 +0800

    inner 1927 - avoid timeout heartbeat connection remaining

--- src/main/java/com/actiontech/dble/backend/heartbeat/HeartbeatSQLJob.java
+++ src/main/java/com/actiontech/dble/backend/heartbeat/HeartbeatSQLJob.java
@@ -20,6 +20,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicStampedReference;
 
 public class HeartbeatSQLJob implements ResponseHandler {
 
@@ -27,9 +28,14 @@ public class HeartbeatSQLJob implements ResponseHandler {
 
     private final String sql;
     private final SQLJobHandler jobHandler;
-    private BackendConnection connection;
-    private AtomicBoolean finished = new AtomicBoolean(false);
-    private MySQLHeartbeat heartbeat;
+    /*
+     *   (null, 0) -> initial
+     *   (conn, 1) -> heartbeat
+     *   (null, 2) -> quit
+     */
+    private final AtomicStampedReference<BackendConnection> connectionRef = new AtomicStampedReference<>(null, 0);
+    private final AtomicBoolean finished = new AtomicBoolean(false);
+    private final MySQLHeartbeat heartbeat;
 
     public HeartbeatSQLJob(MySQLHeartbeat heartbeat, SQLJobHandler jobHandler) {
         super();
@@ -39,16 +45,29 @@ public class HeartbeatSQLJob implements ResponseHandler {
     }
 
     public void terminate() {
-        if (connection != null && !connection.isClosed()) {
+        if (connectionRef.compareAndSet(null, null, 0, 2)) {
+            LOGGER.info("[heartbeat]terminate timeout heartbeat job.");
+            return;
+        }
+
+        final BackendConnection con = this.connectionRef.getReference();
+        connectionRef.set(null, 2);
+        if (con != null && !con.isClosed()) {
             String errMsg = heartbeat.getMessage() == null ? "heart beat quit" : heartbeat.getMessage();
-            LOGGER.info("[heartbeat]terminate this job reason:" + errMsg + " con:" + connection + " sql " + this.sql);
-            connection.businessClose("[heartbeat] quit");
+            LOGGER.info("[heartbeat]terminate this job reason:" + errMsg + " con:" + con + " sql " + this.sql);
+            con.businessClose("[heartbeat] quit");
         }
     }
 
     @Override
     public void connectionAcquired(final BackendConnection conn) {
-        this.connection = conn;
+        if (!connectionRef.compareAndSet(null, conn, 0, 1)) {
+            String errMsg = "[heartbeat]timeout connection[id=" + conn.getId() + "] is acquired, but the conn is useless.";
+            LOGGER.info(errMsg);
+            conn.businessClose(errMsg);
+            return;
+        }
+
         conn.getBackendService().setResponseHandler(this);
         conn.getBackendService().setComplexQuery(true);
         try {
@@ -66,21 +85,28 @@ public class HeartbeatSQLJob implements ResponseHandler {
     public void execute() {
         // reset
         finished.set(false);
-        if (connection == null) {
-            LOGGER.warn("[heartbeat]connect timeout,please pay attention to network latency or packet loss.");
-            heartbeat.setErrorResult("connect timeout");
-            doFinished(true);
-        } else {
+        final BackendConnection conn = connectionRef.getReference();
+        if (conn != null) {
             try {
                 if (LOGGER.isDebugEnabled()) {
-                    LOGGER.debug("[heartbeat]do heartbeat,conn is " + connection);
+                    LOGGER.debug("[heartbeat]do heartbeat,conn is {}", conn);
                 }
-                connection.getBackendService().query(sql);
+                conn.getBackendService().query(sql);
             } catch (Exception e) { // (UnsupportedEncodingException e) {
                 LOGGER.warn("[heartbeat]send heartbeat error", e);
                 heartbeat.setErrorResult("send heartbeat error, because of [" + e.getMessage() + "]");
                 doFinished(true);
             }
+            return;
+        }
+
+        // heartbeat connection had been closed
+        if (connectionRef.getStamp() == 2) {
+            LOGGER.info("[heartbeat]connection had been closed.");
+        } else {
+            LOGGER.warn("[heartbeat]connect timeout,please pay attention to network latency or packet loss.");
+            heartbeat.setErrorResult("connect timeout");
+            doFinished(true);
         }
     }
 
@@ -92,7 +118,7 @@ public class HeartbeatSQLJob implements ResponseHandler {
 
     @Override
     public void connectionError(Throwable e, Object attachment) {
-        LOGGER.warn("[heartbeat]can't get connection for sql :" + sql, e);
+        LOGGER.warn("[heartbeat]can't get connection for sql : {}", sql, e);
         heartbeat.setErrorResult("heartbeat connection Error");
         doFinished(true);
     }
@@ -105,7 +131,6 @@ public class HeartbeatSQLJob implements ResponseHandler {
         LOGGER.warn("[heartbeat]error response errNo: {}, {} from of sql: {} at con: {} db user = {}",
                 errPg.getErrNo(), new String(errPg.getMessage()), sql, service,
                 responseService.getConnection().getInstance().getConfig().getUser());
-
         heartbeat.setErrorResult(new String(errPg.getMessage()));
         if (!((MySQLResponseService) service).syncAndExecute()) {
             service.getConnection().businessClose("[heartbeat]unfinished sync");
@@ -142,7 +167,7 @@ public class HeartbeatSQLJob implements ResponseHandler {
 
     @Override
     public void connectionClose(@NotNull AbstractService service, String reason) {
-        LOGGER.warn("[heartbeat]conn for sql[" + sql + "] is closed, due to " + reason + ", we will try again immediately");
+        LOGGER.warn("[heartbeat]conn for sql[{}] is closed, due to {}, we will try again immediately", sql, reason);
         if (!heartbeat.doHeartbeatRetry()) {
             heartbeat.setErrorResult("heartbeat conn for sql[" + sql + "] is closed, due to " + reason);
             doFinished(true);
@@ -151,7 +176,10 @@ public class HeartbeatSQLJob implements ResponseHandler {
 
     @Override
     public String toString() {
-        return "HeartbeatSQLJob [sql=" + sql + ",  jobHandler=" + jobHandler + ", backend conn" + connection + "]";
+        return "HeartbeatSQLJob [sql=" + sql + ", isQuit=" + isQuit() + ",  jobHandler=" + jobHandler + ", backend conn" + connectionRef.getReference() + "]";
     }
 
+    public boolean isQuit() {
+        return connectionRef.getStamp() == 2;
+    }
 }

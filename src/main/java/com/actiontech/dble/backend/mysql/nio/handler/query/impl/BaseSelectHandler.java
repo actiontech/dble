e780e0c64b3c74b0cd6c82e915c5ca473ae01890
ylinzhu <46126022+ylinzhu@users.noreply.github.com>
2022-11-15 10:03:31 +0800

    fix npe when the frontend connection is closed, the statement is still send (#3470)

--- src/main/java/com/actiontech/dble/backend/mysql/nio/handler/query/impl/BaseSelectHandler.java
+++ src/main/java/com/actiontech/dble/backend/mysql/nio/handler/query/impl/BaseSelectHandler.java
@@ -25,6 +25,7 @@ import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
@@ -50,7 +51,8 @@ public class BaseSelectHandler extends BaseDMLHandler {
 
     public BackendConnection initConnection() throws Exception {
         if (serverSession.closed()) {
-            return null;
+            LOGGER.warn(" FrontendConnection is closed without sending a statement, conn is " + serverSession.getShardingService().getConnection());
+            throw new IOException("FrontendConnection is closed");
         }
 
         BackendConnection exeConn = serverSession.getTarget(rrss);

164229f3ea8b0dd9e17734b7afac7de3d2e9e4af
baofengqi <baofengqi@actionsky.com>
2022-11-24 15:01:36 +0800

    inner 1959 - avoid the case that connectionClose thread is updating flag but other thread had finished

--- src/main/java/com/actiontech/dble/backend/mysql/nio/handler/MultiNodeHandler.java
+++ src/main/java/com/actiontech/dble/backend/mysql/nio/handler/MultiNodeHandler.java
@@ -7,13 +7,11 @@ package com.actiontech.dble.backend.mysql.nio.handler;
 
 import com.actiontech.dble.config.ErrorCode;
 import com.actiontech.dble.net.mysql.ErrorPacket;
-import com.actiontech.dble.net.service.AbstractService;
 import com.actiontech.dble.route.RouteResultsetNode;
 import com.actiontech.dble.server.NonBlockingSession;
 import com.actiontech.dble.services.mysqlsharding.MySQLResponseService;
 import com.actiontech.dble.util.StringUtil;
 import com.google.common.collect.Sets;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,6 +24,7 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public abstract class MultiNodeHandler implements ResponseHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(MultiNodeHandler.class);
+
     protected final ReentrantLock lock = new ReentrantLock();
     protected final NonBlockingSession session;
     protected final AtomicBoolean errorResponse = new AtomicBoolean(false);
@@ -53,51 +52,6 @@ public abstract class MultiNodeHandler implements ResponseHandler {
         } else {
             error = error + "\n" + errMsg;
         }
-    }
-
-    @Override
-    public void connectionError(Throwable e, Object attachment) {
-        this.setFail("backend connect: " + e);
-        LOGGER.info("backend connect", e);
-        boolean finished;
-        lock.lock();
-        try {
-            errorConnsCnt++;
-            finished = canResponse();
-        } finally {
-            lock.unlock();
-        }
-        this.tryErrorFinished(finished);
-    }
-
-
-    public boolean clearIfSessionClosed(NonBlockingSession nonBlockingSession) {
-        if (nonBlockingSession.closed()) {
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("session closed ,clear resources " + nonBlockingSession);
-            }
-            nonBlockingSession.clearResources(true);
-            this.clearResources();
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    protected boolean[] decrementToZeroAndCheckNode(MySQLResponseService service) {
-        boolean zeroReached = false;
-        boolean justRemoved;
-        lock.lock();
-        try {
-            RouteResultsetNode rNode = (RouteResultsetNode) service.getAttachment();
-            justRemoved = unResponseRrns.remove(rNode);
-            if (justRemoved) {
-                zeroReached = canResponse();
-            }
-        } finally {
-            lock.unlock();
-        }
-        return new boolean[]{zeroReached, justRemoved};
     }
 
     protected boolean decrementToZero(MySQLResponseService service) {
@@ -149,7 +103,7 @@ public abstract class MultiNodeHandler implements ResponseHandler {
         if (allEnd && !session.closed()) {
             // clear session resources,release all
             if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("error all end ,clear session resource ");
+                LOGGER.debug("error all end,clear session resource");
             }
             clearSessionResources();
             if (errorResponse.compareAndSet(false, true)) {
@@ -158,25 +112,26 @@ public abstract class MultiNodeHandler implements ResponseHandler {
         }
     }
 
-    protected void clearSessionResources() {
+    public boolean clearIfSessionClosed(NonBlockingSession nonBlockingSession) {
+        if (nonBlockingSession.closed()) {
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("session closed ,clear resources {}", nonBlockingSession);
+            }
+            nonBlockingSession.clearResources(true);
+            this.clearResources();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void clearSessionResources() {
         if (session.getShardingService().isAutocommit()) {
             session.closeAndClearResources(error);
         } else {
             session.getShardingService().setTxInterrupt(this.error);
             this.clearResources();
         }
-    }
-
-    @Override
-    public void connectionClose(@NotNull AbstractService service, String reason) {
-        this.setFail("closed connection:" + reason + " con:" + service.toString());
-        if (error == null) {
-            error = "back connection closed ";
-        }
-        RouteResultsetNode rNode = (RouteResultsetNode) ((MySQLResponseService) service).getAttachment();
-        session.getTargetMap().remove(rNode);
-        ((MySQLResponseService) service).setResponseHandler(null);
-        this.tryErrorFinished(decrementToZero((MySQLResponseService) service));
     }
 
     public void clearResources() {

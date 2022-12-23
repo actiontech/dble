b569e3980839f82f17b27a8049586729698cf265
Rico <dcy10000@gmail.com>
2022-12-23 09:37:33 +0800

    inner-1945-supplement: report error packet when connection closed (#3562) inner-2062: fix npe
    Signed-off-by: dcy10000 <dcy10000@gmail.com>
    
    Signed-off-by: dcy10000 <dcy10000@gmail.com>
    (cherry picked from commit 74e2023d1fe8321a36251e16ca1a8e3be16626df)
    Signed-off-by: dcy10000 <dcy10000@gmail.com>

--- src/main/java/com/actiontech/dble/services/BackendService.java
+++ src/main/java/com/actiontech/dble/services/BackendService.java
@@ -16,7 +16,9 @@ import com.actiontech.dble.net.mysql.ErrorPacket;
 import com.actiontech.dble.net.mysql.OkPacket;
 import com.actiontech.dble.net.response.ProtocolResponseHandler;
 import com.actiontech.dble.net.service.AbstractService;
+import com.actiontech.dble.net.service.NormalServiceTask;
 import com.actiontech.dble.net.service.ServiceTask;
+import com.actiontech.dble.net.service.ServiceTaskType;
 import com.actiontech.dble.route.parser.util.Pair;
 import com.actiontech.dble.singleton.TraceManager;
 import com.actiontech.dble.statistic.stat.ThreadWorkUsage;
@@ -167,6 +169,18 @@ public abstract class BackendService extends AbstractService {
         }
     }
 
+    void parseErrorPacket(byte[] data, String reason) {
+        try {
+            ErrorPacket errPkg = new ErrorPacket();
+            errPkg.read(data);
+            String errMsg = "errNo:" + errPkg.getErrNo() + " " + new String(errPkg.getMessage());
+            LOGGER.warn("no handler process the execute packet err,sql error:{},back service:{},from reason:{}", errMsg, this, reason);
+
+        } catch (RuntimeException e) {
+            LOGGER.info("error handle error-packet", e);
+        }
+    }
+
     /**
      * handle mysql packet returned from backend mysql
      *
@@ -174,6 +188,9 @@ public abstract class BackendService extends AbstractService {
      */
     protected void handleInnerData(byte[] data) {
         if (connection.isClosed()) {
+            if (data != null && data.length > 4 && data[4] == ErrorPacket.FIELD_COUNT) {
+                parseErrorPacket(data, "connection close");
+            }
             return;
         }
 
@@ -201,15 +218,27 @@ public abstract class BackendService extends AbstractService {
         LOGGER.warn(this.toString() + " handle data error:", e);
         connection.close("handle data error:" + e.getMessage());
         while (taskQueue.size() > 0) {
-            taskQueue.clear();
+            clearTaskQueue();
             // clear all data from the client
             LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(1000));
         }
     }
 
+    private void clearTaskQueue() {
+        ServiceTask task;
+        while ((task = taskQueue.poll()) != null) {
+            if (task.getType() == ServiceTaskType.NORMAL) {
+                final byte[] data = ((NormalServiceTask) task).getOrgData();
+                if (data != null && data.length > 4 && data[4] == ErrorPacket.FIELD_COUNT) {
+                    parseErrorPacket(data, "cleanup");
+                }
+            }
+        }
+    }
+
     @Override
     public void cleanup() {
-        this.taskQueue.clear();
+        clearTaskQueue();
         backendSpecialCleanUp();
         TraceManager.sessionFinish(this);
     }

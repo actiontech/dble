070719b7448ecdd0a1fcf9ce29209cab80bf59c8
dcy10000 <dcy10000@gmail.com>
2023-03-20 13:53:45 +0800

    Revert "inner-2140: fix the multi-thread bug for xa"
    This reverts commit 67510c841fec55c04ef3ac9eb619e54f67b403cc.

--- src/main/java/com/actiontech/dble/backend/mysql/nio/handler/transaction/xa/XAHandler.java
+++ src/main/java/com/actiontech/dble/backend/mysql/nio/handler/transaction/xa/XAHandler.java
@@ -14,7 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
@@ -25,7 +24,7 @@ public class XAHandler {
     private static final String XARECOVER_SQL = "XA RECOVER";
     private static final String KILL_SQL = "KILL CONNECTION ";
     private static final String[] MYSQL_RECOVER_COLS = new String[]{"formatID", "gtrid_length", "bqual_length", "data"};
-    private Map<PhysicalDbInstance, List<Map<String, String>>> results = new ConcurrentHashMap<>(8);
+    private Map<PhysicalDbInstance, List<Map<String, String>>> results = new HashMap<>(8);
     private List<SQLJob> sqlJobs = new ArrayList<>();
     private final AtomicInteger count = new AtomicInteger();
     private Lock lock;

3f2a420b023341fefef56f30778a272994ecbc5f
wenyh <44251917+wenyh1@users.noreply.github.com>
2023-05-08 14:31:05 +0800

    [inner-2213] adjust the basis of whether the 'readInstance' judgment (cherry pick from #3681)

--- src/main/java/com/actiontech/dble/net/connection/BackendConnection.java
+++ src/main/java/com/actiontech/dble/net/connection/BackendConnection.java
@@ -179,6 +179,10 @@ public class BackendConnection extends PooledConnection {
         return instance;
     }
 
+    public boolean isFromSlaveDB() {
+        return instance.isReadInstance();
+    }
+
     @Override
     public String toString() {
         return "BackendConnection[id = " + id + " host = " + host + " port = " + port + " localPort = " + localPort + " mysqlId = " + threadId + " db config = " + instance;
--- src/main/java/com/actiontech/dble/net/connection/PooledConnection.java
+++ src/main/java/com/actiontech/dble/net/connection/PooledConnection.java
@@ -104,10 +104,6 @@ public abstract class PooledConnection extends AbstractConnection {
         this.poolRelated = poolRelated;
     }
 
-    public boolean isFromSlaveDB() {
-        return poolRelated.isFromSlave();
-    }
-
     public String getOldSchema() {
         return oldSchema;
     }

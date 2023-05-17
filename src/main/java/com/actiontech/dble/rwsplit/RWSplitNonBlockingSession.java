06cf9a77ebed2cf6e88f6da7542624e32970e0f8
ylinzhu <46126022+ylinzhu@users.noreply.github.com>
2023-05-17 14:30:57 +0800

    fix jdbc compatible pre-delivery statements error inner 2212
    fix jdbc compatible pre-delivery statements error inner 2212

--- src/main/java/com/actiontech/dble/rwsplit/RWSplitNonBlockingSession.java
+++ src/main/java/com/actiontech/dble/rwsplit/RWSplitNonBlockingSession.java
@@ -9,6 +9,7 @@ import com.actiontech.dble.DbleServer;
 import com.actiontech.dble.backend.datasource.PhysicalDbGroup;
 import com.actiontech.dble.backend.datasource.PhysicalDbInstance;
 import com.actiontech.dble.backend.mysql.ByteUtil;
+import com.actiontech.dble.backend.mysql.nio.handler.RwSplitSelectVariablesHandler;
 import com.actiontech.dble.config.ErrorCode;
 import com.actiontech.dble.config.model.SystemConfig;
 import com.actiontech.dble.config.util.ConfigException;
@@ -202,6 +203,38 @@ public class RWSplitNonBlockingSession extends Session {
             return true;
         }
         return writeStatistical;
+    }
+
+    /**
+     * jdbc compatible pre-delivery statements
+     *
+     * @param master
+     * @param originPacket
+     * @param callback
+     * @param writeStatistical
+     */
+    public void selectCompatibilityVariables(Boolean master, byte[] originPacket, Callback callback, boolean writeStatistical) {
+        try {
+            RWSplitHandler handler = getRwSplitSelectVariablesHandler(originPacket, callback);
+            if (handler == null) return;
+            getConnection(handler, master, isWriteStatistical(writeStatistical), false);
+        } catch (SQLSyntaxErrorException | IOException se) {
+            rwSplitService.writeErrMessage(ErrorCode.ER_UNKNOWN_ERROR, se.getMessage());
+        }
+    }
+
+    @Nullable
+    private RWSplitHandler getRwSplitSelectVariablesHandler(byte[] originPacket, Callback callback) throws SQLSyntaxErrorException, IOException {
+        if (conn != null && !conn.isClosed()) {
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("select bind conn[id={}]", conn.getId());
+            }
+            RWSplitHandler handler = new RwSplitSelectVariablesHandler(rwSplitService, originPacket, callback, false);
+            checkDest(!conn.getInstance().isReadInstance());
+            handler.execute(conn);
+            return null;
+        }
+        return new RwSplitSelectVariablesHandler(rwSplitService, originPacket, callback, false);
     }
 
     private Boolean canRunOnMaster(Boolean master) {

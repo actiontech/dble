af29c6ba96a1ff66f387091f6503b58bc34c4f07
wenyh <44251917+wenyh1@users.noreply.github.com>
2023-05-18 15:58:11 +0800

    [inner-2227] prevents 'connection-pool-evictor-thread' thread from exiting unexpectedly

--- src/main/java/com/actiontech/dble/backend/pool/ConnectionPool.java
+++ src/main/java/com/actiontech/dble/backend/pool/ConnectionPool.java
@@ -517,6 +517,8 @@ public class ConnectionPool extends PoolBase implements PooledConnectionListener
 
                 // Try to maintain minimum connections
                 fillPool();
+            } catch (Throwable t) {
+                LOGGER.warn("Evictor.run happen Throwable: ", t);
             } finally {
                 // Restore the previous CCL
                 Thread.currentThread().setContextClassLoader(savedClassLoader);
@@ -530,6 +532,7 @@ public class ConnectionPool extends PoolBase implements PooledConnectionListener
          */
         void setScheduledFuture(final ScheduledFuture<?> scheduledFuture) {
             this.scheduledFuture = scheduledFuture;
+            EvictionTimer.getAliveEvictor().add(scheduledFuture);
         }
 
         /**
@@ -537,6 +540,7 @@ public class ConnectionPool extends PoolBase implements PooledConnectionListener
          */
         void cancel() {
             scheduledFuture.cancel(false);
+            EvictionTimer.getAliveEvictor().remove(scheduledFuture);
         }
     }
 }

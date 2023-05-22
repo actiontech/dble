511c0292add0198220a124f6a28cc76ec3800e1c
wenyh <44251917+wenyh1@users.noreply.github.com>
2023-05-22 17:51:45 +0800

    [inner-2222] clear the 'sendData' resource in the transaction correctly (cherry pick from #3702)

--- src/main/java/com/actiontech/dble/backend/mysql/nio/handler/transaction/TransactionHandlerManager.java
+++ src/main/java/com/actiontech/dble/backend/mysql/nio/handler/transaction/TransactionHandlerManager.java
@@ -60,6 +60,10 @@ public class TransactionHandlerManager {
         }
     }
 
+    public NormalTransactionHandler getNormalTransactionHandler() {
+        return (NormalTransactionHandler) normalHandler;
+    }
+
     public void commit() {
         if (xaTxId != null) {
             xaHandler.commit();

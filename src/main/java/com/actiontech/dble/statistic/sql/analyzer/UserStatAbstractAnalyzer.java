3e8d20740d801244d1105975f72d4539100b9cb1
wenyh <44251917+wenyh1@users.noreply.github.com>
2023-08-23 13:54:30 +0800

    atomic processing

--- src/main/java/com/actiontech/dble/statistic/sql/analyzer/UserStatAbstractAnalyzer.java
+++ src/main/java/com/actiontech/dble/statistic/sql/analyzer/UserStatAbstractAnalyzer.java
@@ -30,10 +30,10 @@ public final class UserStatAbstractAnalyzer implements AbstractAnalyzer {
 
     @Override
     public void toAnalyzing(final StatisticFrontendSqlEntry fEntry) {
-        UserStat userStat;
-        if ((userStat = userStatMap.get(fEntry.getFrontend().getUser())) == null) {
-            userStat = new UserStat(fEntry.getFrontend());
-            userStatMap.put(fEntry.getFrontend().getUser(), userStat);
+        UserStat newUserStat = new UserStat(fEntry.getFrontend());
+        UserStat userStat = userStatMap.putIfAbsent(fEntry.getFrontend().getUser(), newUserStat);
+        if (userStat == null) {
+            userStat = newUserStat;
         }
         userStat.update(fEntry);
     }

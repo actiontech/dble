663d652f3607c3f448f60ffe2eecf806a348273d
dcy <dcy10000@gmail.com>
2024-06-11 15:21:25 +0800

    inner-2396:fix delayThreshold not work properly

--- src/main/java/com/actiontech/dble/backend/datasource/PhysicalDbInstance.java
+++ src/main/java/com/actiontech/dble/backend/datasource/PhysicalDbInstance.java
@@ -391,7 +391,7 @@ public abstract class PhysicalDbInstance implements ReadTimeStatusInstance {
             return false;
         }
         boolean isSync = dbSynStatus == MySQLHeartbeat.DB_SYN_NORMAL;
-        boolean isNotDelay = slaveBehindMaster <= this.dbGroupConfig.getDelayThreshold();
+        boolean isNotDelay = slaveBehindMaster <= (this.dbGroupConfig.getDelayThreshold() / 1000);
         return isSync && isNotDelay;
     }
 

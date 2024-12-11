6246b8fff6264ceacab1156cb985078a2c58fd52
dcy <dcy10000@gmail.com>
2024-12-11 10:47:27 +0800

    inner-2413: recreate heartbeat connection when timeout

--- src/main/java/com/actiontech/dble/config/model/SystemConfig.java
+++ src/main/java/com/actiontech/dble/config/model/SystemConfig.java
@@ -95,6 +95,7 @@ public final class SystemConfig {
     private long idleTimeout = DEFAULT_IDLE_TIMEOUT;
     // sql execute timeout (second)
     private long sqlExecuteTimeout = 300;
+    private long heartbeatSqlExecuteTimeout = 10;
     // connection will force close if received close packet but haven't been closed after closeTimeout milliseconds.
     // set the value too big is not a good idea.
     private long closeTimeout = 100;
@@ -990,6 +991,14 @@ public final class SystemConfig {
         }
     }
 
+
+    public long getHeartbeatSqlExecuteTimeout() {
+        return heartbeatSqlExecuteTimeout;
+    }
+
+    public void setHeartbeatSqlExecuteTimeout(long heartbeatSqlExecuteTimeout) {
+        this.heartbeatSqlExecuteTimeout = heartbeatSqlExecuteTimeout;
+    }
 
     public int getTxIsolation() {
         return txIsolation;
@@ -2039,6 +2048,7 @@ public final class SystemConfig {
                 ", tcpKeepIdle=" + tcpKeepIdle +
                 ", tcpKeepInterval=" + tcpKeepInterval +
                 ", tcpKeepCount=" + tcpKeepCount +
+                ", heartbeatSqlExecuteTimeout=" + heartbeatSqlExecuteTimeout +
                 "]";
     }
 
